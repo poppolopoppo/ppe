a45de8ef914c935fb00b8b3190b8b02ab8b41a23
poppolopoppo <frostiebek@gmail.com>
2014-11-03 01:50:04 +0100

    Doc: added support for GraphViz
    - GraphViz2.38 must be installed on the machine though
    - if not found doxygen will still continue, but graphs won't be available
    - it takes ~2 minutes to generate all the doc
    - doc if phat (~400mo with all images !) and can be entirely generated -> won't be commit on the repo

--- doxygen.bat
+++ doxygen.bat
@@ -1,4 +1,7 @@
-REM launch build of documentation through doxygen
+@REM launch build of documentation through doxygen
+
+@REM should install Graphviz2.38
+SET PATH=C:\Program Files (x86)\Graphviz2.38\bin\;%PATH%
 
 cd "%~d0\%~p0\Doc"
 "%~d0\%~p0\Doc\doxygen.exe" %*
