0f0c747f34f29ec244184d037b8e438fe7df5cbd
poppolopoppo <frostiebek@gmail.com>
2014-11-12 04:11:47 +0100

    FBuild: dist-clean also cleans fbuild and visual studio files

--- Output/dist_clean.bat
+++ Output/dist_clean.bat
@@ -3,9 +3,14 @@
 
 echo [%DATE%] > "%~d0\%~p0\dist_clean.log"
 
-FOR %%D IN (Binary Intermediate Resources Unity) DO (
-    echo Deleting '%~d0%~p0%%D' ... >> "%~d0\%~p0\dist_clean.log"
+FOR %%D IN (Binary Intermediate Resources Unity ..\Doc\Doxygen) DO (
+    echo Deleting directory '%~d0%~p0%%D' >> "%~d0\%~p0\dist_clean.log"
     rmdir /S /Q "%~d0\%~p0\%%D" >> "%~d0\%~p0\dist_clean.log"
 )
+
+cd %~d0%~p0.. && FOR /F %%F IN ('dir /b/s *.suo *.sdf *.fdb') DO (
+    echo Deleting file '%%F' >> "%~d0\%~p0\dist_clean.log"
+    del "%%F" >> "%~d0\%~p0\dist_clean.log"
+) && cd %~d0%~p0
 
 REM end
