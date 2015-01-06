6431db7fb3d3f7c8bc26e661b8eeb911031eae83
poppolopoppo <frostiebek@gmail.com>
2015-01-06 18:08:22 +0100

    Color: colors are set to 0 in the ctor
    - instead of leaving them uninitialized

--- Source/Core/Color/Color-inl.h
+++ Source/Core/Color/Color-inl.h
@@ -7,7 +7,7 @@ namespace Core {
 //////////////////////////////////////////////////////////////////////////////
 //----------------------------------------------------------------------------
 template <typename T, typename _Shuffle>
-BasicColor<T, _Shuffle>::BasicColor() {}
+BasicColor<T, _Shuffle>::BasicColor() : _data(T(0)) {}
 //----------------------------------------------------------------------------
 template <typename T, typename _Shuffle>
 BasicColor<T, _Shuffle>::~BasicColor() {}
