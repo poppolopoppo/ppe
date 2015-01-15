c8b6136f4a2ff96e686bd341ad5682941f5eb2f0
poppolopoppo <frostiebek@gmail.com>
2015-01-15 23:07:00 +0100

    Mesh: finished support for WaveFront OBJ (added a MeshGeneration domain)

--- Source/Core.Engine/Mesh/Loader/ModelLoader.h
+++ Source/Core.Engine/Mesh/Loader/ModelLoader.h
@@ -2,14 +2,19 @@
 
 #include "Core.Engine/Engine.h"
 
+#include "Core/Allocator/Allocation.h"
 #include "Core/Memory/RefPtr.h"
 
 namespace Core {
 class Filename;
+template <typename T, typename _Allocator>
+class RawStorage;
 namespace Engine {
 FWD_REFPTR(Model);
 //----------------------------------------------------------------------------
 //////////////////////////////////////////////////////////////////////////////
+//----------------------------------------------------------------------------
+typedef RawStorage<char, THREAD_LOCAL_ALLOCATOR(MeshGeneration, char)> ModelStream;
 //----------------------------------------------------------------------------
 bool LoadModel(PModel& pModel, const Filename& filename);
 //----------------------------------------------------------------------------
