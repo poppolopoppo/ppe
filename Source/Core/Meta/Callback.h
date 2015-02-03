07dd9b1bfb2eb1cb2f8a1db3f39e66dccefa6558
poppolopoppo <frostiebek@gmail.com>
2015-02-03 01:07:08 +0100

    Meta: Callback helper
    - substitute to std::function voluntary nerfed

--- Source/Core/Meta/Callback.h
+++ Source/Core/Meta/Callback.h
@@ -7,34 +7,56 @@ namespace Meta {
 //----------------------------------------------------------------------------
 //////////////////////////////////////////////////////////////////////////////
 //----------------------------------------------------------------------------
+template <typename T>
+struct CallbackImpl {
+    enum : bool { enabled = false };
+};
+//----------------------------------------------------------------------------
 template <typename _Return, typename... _Args>
-class Callback {
-public:
-    typedef _Return (*helper_type)(void *, const _Args&... );
+struct CallbackImpl<_Return (*)(_Args...)> {
+    enum : bool { enabled = true };
 
-    template <typename T, _Return (T::*_Member)(const _Args&&... )>
-    struct Bind {
-        static helper_type helper() { return &call; }
-        static _Return call(void *powner, const _Args& args... ) {
-            return reinterpret_cast<T *>(powner)->*_Member(args...);
-        }
-    };
+    typedef _Return (*helper_func)(void *powner, _Args... args);
 
-    Callback() : _powner(nullptr), _call(nullptr) {}
-    Callback(void *powner, helper_type call) 
-    :   _powner(powner), _call(call) {}
+    void *_powner;
+    helper_func _fhelper;
 
-    operator helper_type () const { return _call; }
+    operator bool () const { return _fhelper != nullptr; }
+    _Return operator ()(_Args... args) const { return _fhelper(_powner, args...); }
 
-    _Return operator ()(const _Args& args...) const {
-        Assert(_powner);
-        Assert(_call);
-        return _call(_powner, args...);
+    template <typename T, _Return (T::*_Member)(_Args...)>
+    void bind(T *powner) {
+        assert(powner);
+        _powner = powner;
+        _fhelper = &call_<T, _Member>;
     }
 
-private:
-    void *_powner;
-    helper_type _call;
+    template <typename T, _Return (T::*_Member)(_Args...)>
+    static _Return call_(void *powner, _Args... args) {
+        assert(nullptr != powner);
+        return (reinterpret_cast<T *>(powner)->*_Member)(args...);
+    }
+
+    template <_Return (*_Function)(_Args...)>
+    void bind() {
+        _powner = nullptr;
+        _fhelper = &call_<_Function>;
+    }
+
+    template <_Return (*_Function)(_Args...)>
+    static _Return call_(void *powner, _Args... args) {
+        assert(nullptr == powner);
+        return (*_Function)(args...);
+    }
+};
+//----------------------------------------------------------------------------
+template <typename _Function>
+struct Callback : CallbackImpl<_Function> {
+    typedef CallbackImpl<_Function> impl_type;
+    static_assert(impl_type::enabled, "unknown function type");
+    using impl_type::operator bool;
+    using impl_type::operator ();
+    using impl_type::bind;
 };
 //----------------------------------------------------------------------------
 //////////////////////////////////////////////////////////////////////////////
