44fac2b0ae55baf1cf5e744201ba0a2394e18750
poppolopoppo <frostiebek@gmail.com>
2015-07-14 22:28:07 +0200

    Core: allocation operators overloading cannot be inline

--- Source/Core/Allocator/New.h
+++ Source/Core/Allocator/New.h
@@ -2,30 +2,28 @@
 
 #include "Core/Core.h"
 
-#include "Core/Allocator/Malloc.h"
-
 //----------------------------------------------------------------------------
 //////////////////////////////////////////////////////////////////////////////
 //----------------------------------------------------------------------------
-FORCE_INLINE CORE_RETURN_NOT_NULL void *operator new(size_t size) { return Core::malloc(size); }
+CORE_RETURN_NOT_NULL void *operator new(size_t size);
 //----------------------------------------------------------------------------
-FORCE_INLINE CORE_RETURN_MAYBE_NULL void *operator new(size_t size, std::nothrow_t const& ) throw()  { return Core::malloc(size); }
+CORE_RETURN_MAYBE_NULL void *operator new(size_t size, std::nothrow_t const& ) throw();
 //----------------------------------------------------------------------------
-FORCE_INLINE CORE_RETURN_NOT_NULL void *operator new[](size_t size)  { return Core::malloc(size); }
+CORE_RETURN_NOT_NULL void *operator new[](size_t size);
 //----------------------------------------------------------------------------
-FORCE_INLINE CORE_RETURN_MAYBE_NULL void *operator new[](size_t size,std::nothrow_t const& ) throw() { return Core::malloc(size); }
+CORE_RETURN_MAYBE_NULL void *operator new[](size_t size,std::nothrow_t const& ) throw();
 //----------------------------------------------------------------------------
-FORCE_INLINE void operator delete(void* block) throw() { Core::free(block); }
-FORCE_INLINE void operator delete(void* block, std::nothrow_t const& ) throw() { Core::free(block); }
+void operator delete(void* block) throw();
+void operator delete(void* block, std::nothrow_t const& ) throw();
 //----------------------------------------------------------------------------
-FORCE_INLINE void operator delete[](void* block) throw() { Core::free(block); }
-FORCE_INLINE void operator delete[](void* block, std::nothrow_t const& ) throw() { Core::free(block); }
+void operator delete[](void* block) throw();
+void operator delete[](void* block, std::nothrow_t const& ) throw();
 //----------------------------------------------------------------------------
-FORCE_INLINE void operator delete(void*  block, size_t/* size */) throw() { Core::free(block); }
-FORCE_INLINE void operator delete[](void* block, size_t/* size */) throw() { Core::free(block); }
+void operator delete(void*  block, size_t/* size */) throw();
+void operator delete[](void* block, size_t/* size */) throw();
 //----------------------------------------------------------------------------
 #if (defined(_MSC_VER) && (_MSC_VER != 1400))
-FORCE_INLINE CORE_RETURN_NOT_NULL void *operator new(size_t size, int/* nBlockUse */, const char* /* szFileName */, int/* nLine */) { return Core::malloc(size); }
+CORE_RETURN_NOT_NULL void *operator new(size_t size, int/* nBlockUse */, const char* /* szFileName */, int/* nLine */);
 #endif
 //----------------------------------------------------------------------------
 //////////////////////////////////////////////////////////////////////////////
