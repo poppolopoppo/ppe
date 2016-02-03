61ed6fd027e0556b76d8a049fb8c146eccfcf1f4
PopPoLoPoPpo <frostiebek@gmail.com>
2016-02-03 03:29:27 +0100

    Memory: MemoryView::data()

--- Source/Core/Memory/MemoryView.h
+++ Source/Core/Memory/MemoryView.h
@@ -48,6 +48,7 @@ public:
     pointer Pointer() const { return _storage; }
     size_t SizeInBytes() const { return _size * sizeof(T); }
 
+    pointer data() const { return _storage; }
     size_type size() const { return _size; }
     bool empty() const { return 0 == _size; }
 
