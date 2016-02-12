e7dc30745b3abe7a73f071a3155bef3572a20444
PopPoLoPoPpo <frostiebek@gmail.com>
2016-02-12 16:37:32 +0100

    RobotApp: new tests

--- Source/Core.ContentGenerator/RobotApp.cpp
+++ Source/Core.ContentGenerator/RobotApp.cpp
@@ -12,6 +12,7 @@
 #include "Core/Maths/RandomGenerator.h"
 #include "Core/Memory/Compression.h"
 #include "Core/Memory/MemoryStream.h"
+#include "Core/Time/TimedScope.h"
 
 #include "Core.Graphics/Device/DeviceAPI.h"
 #include "Core.Graphics/Device/DeviceEncapsulator.h"
@@ -33,6 +34,10 @@
 #include "Core/Container/BurstTrie.h"
 
 #include "Core.RTTI/MetaType.Definitions-inl.h"
+
+#include "Core/Diagnostic/Profiling.h"
+
+#include <bitset>
 
 namespace Core {
 namespace ContentGenerator {
@@ -273,6 +278,273 @@ void RTTIAtomRandomizer_::Randomize(RTTI::MetaObject* pobject) {
 //----------------------------------------------------------------------------
 //////////////////////////////////////////////////////////////////////////////
 //----------------------------------------------------------------------------
+template <typename _Key, typename _Value, size_t _BulkSize>
+struct BulkTrieNode : public BulkTrieNode<_Key, void , _BulkSize>{
+    _Value Values[_BulkSize];
+
+    BulkTrieNode() {}
+};
+//----------------------------------------------------------------------------
+template <typename _Key, size_t _BulkSize>
+struct BulkTrieNode<_Key, void, _BulkSize> {
+    typedef uint32_t index_type;
+
+    static constexpr index_type NoIndex = index_type(-1);
+
+    _Key Keys[_BulkSize];
+    std::bitset<_BulkSize> HasValue;
+    index_type Left[_BulkSize];
+    index_type Center[_BulkSize];
+    index_type Right[_BulkSize];
+
+    BulkTrieNode() {
+        std::fill(Left, Left+_BulkSize, NoIndex);
+        std::fill(Center, Center+_BulkSize, NoIndex);
+        std::fill(Right, Right+_BulkSize, NoIndex);
+    }
+};
+//----------------------------------------------------------------------------
+template <
+    typename _Key
+,   typename _Value
+,   size_t _BulkSize = 4096
+,   size_t _BucketCount = 11
+,   typename _Less = Meta::Less<_Key>
+,   typename _Equal = Meta::EqualTo<_Key>
+,   typename _Hasher = Hash<_Key>
+,   typename _Allocator = ALLOCATOR(Container, BulkTrieNode<_Key COMMA _Value COMMA _BulkSize>)
+>   class BulkTrie : _Allocator {
+public:
+    typedef _Key key_type;
+    typedef _Value value_type;
+    typedef _Less less_functor;
+    typedef _Equal equal_to_functor;
+    typedef _Hasher hash_functor;
+    typedef _Allocator allocator_type;
+
+    typedef std::allocator_traits<allocator_type> allocator_traits;
+    typedef typename allocator_traits::difference_type difference_type;
+    typedef typename allocator_traits::size_type size_type;
+
+    static constexpr size_type BulkSize = _BulkSize;
+    static constexpr size_type BucketCount = _BucketCount;
+
+    typedef MemoryView<const _Key> sequence_type;
+
+    typedef BulkTrieNode<_Key, _Value, _BulkSize> node_type;
+    typedef typename node_type::index_type index_type;
+    static constexpr index_type NoIndex = node_type::NoIndex;
+    static constexpr index_type RootIndex = 0;
+    typedef Vector<node_type*, typename _Allocator::template rebind<node_type*>::other> node_vector;
+
+    struct Iterator {
+        index_type Bucket;
+        index_type Index;
+    };
+
+    BulkTrie() : _wordCount(0) {}
+    ~BulkTrie() { Clear(); }
+
+    BulkTrie(const BulkTrie&) = delete;
+    BulkTrie& operator=(const BulkTrie&) = delete;
+
+    size_type size() const { return _wordCount; }
+    bool empty() const { return (0 == _wordCount); }
+
+    bool Insert_ReturnIfExists(Iterator* it, const sequence_type& keys) {
+        Assert(nullptr != it);
+        Assert(false == keys.empty());
+
+        it->Bucket = BucketIndex_(keys);
+        Bucket& bucket = _buckets[it->Bucket];
+
+        index_type parent = NoIndex;
+        index_type current = RootIndex;
+
+        const size_type n = keys.size();
+        forrange(i, 0, n) {
+            const _Key& key = keys[i];
+            do {
+                Assert(NoIndex != current);
+
+                index_type rel;
+                node_type* node = nullptr;
+                if (current != bucket.Count) {
+                    node = bucket.Node(&rel, current);
+
+                    const _Key& other = node->Keys[rel];
+                    if (equal_to_functor()(key, other)) {
+                        parent = current;
+                        if (NoIndex == (current = node->Center[rel]) && i + 1 != n)
+                            current = node->Center[rel] = checked_cast<index_type>(bucket.Count);
+                        break;
+                    }
+                    else if (less_functor()(other, key)) {
+                        if (NoIndex == (current = node->Left[rel]))
+                            current = node->Left[rel] = checked_cast<index_type>(bucket.Count);
+                    }
+                    else {
+                        if (NoIndex == (current = node->Right[rel]))
+                            current = node->Right[rel] = checked_cast<index_type>(bucket.Count);
+                    }
+                }
+                else {
+                    ++bucket.Count;
+
+                    if (bucket.Nodes.size()*BulkSize <= current) {
+                        rel = current%BulkSize;
+                        node = allocator_traits::allocate(*this, 1);
+                        Assert(node);
+                        allocator_traits::construct(*this, node);
+                        bucket.Nodes.push_back(node);
+                    }
+                    else {
+                        node = bucket.Node(&rel, current);
+                    }
+
+                    node->Keys[rel] = key;
+
+                    Assert(not node->HasValue.test(rel));
+                    Assert(NoIndex == node->Left[rel]);
+                    Assert(NoIndex == node->Center[rel]);
+                    Assert(NoIndex == node->Right[rel]);
+
+                    parent = current;
+
+                    if (NoIndex == (current = node->Center[rel]) && i + 1 != n)
+                        current = node->Center[rel] = checked_cast<index_type>(bucket.Count);
+
+                    break;
+                }
+            }
+            while (true);
+        }
+
+        Assert(NoIndex != parent);
+        it->Index = parent;
+
+        index_type rel;
+        node_type* const node = bucket.Node(&rel, parent);
+        Assert(keys.back() == node->Keys[rel]);
+
+        if (node->HasValue.test(rel)) {
+            return true;
+        }
+        else {
+            _wordCount++;
+            node->HasValue.set(rel, true);
+            return false;
+        }
+    }
+
+    Iterator Insert_AssertUnique(const sequence_type& keys) {
+        Iterator result;
+        if (Insert_ReturnIfExists(&result, keys))
+            AssertNotReached();
+        return result;
+    }
+
+    bool Find_ReturnIfExists(Iterator* it, const sequence_type& keys, Iterator hint = {NoIndex,NoIndex}) const {
+        Assert(nullptr != it);
+        Assert(false == keys.empty());
+
+        if (0 == _wordCount)
+            return false;
+
+        it->Bucket = (NoIndex != hint.Bucket)
+            ? hint.Bucket
+            : BucketIndex_(keys);
+
+        const Bucket& bucket = _buckets[it->Bucket];
+
+        index_type parent = NoIndex;
+        index_type current = (bucket.Count ? RootIndex : NoIndex);
+
+        if (NoIndex != hint.Index) {
+            index_type rel;
+            const node_type* const node = bucket.Node(&rel, hint.Index);
+            current = node->Center[rel];
+        }
+
+        const size_type n = keys.size();
+        for (size_type i = 0; i < n; ++i) {
+            const _Key key = keys[i]; // local cpy
+            while (NoIndex != current) {
+                index_type rel;
+                node_type* const node = bucket.Node(&rel, current);
+
+                parent = current;
+
+                const _Key& other = node->Keys[rel];
+                if (equal_to_functor()(key, other)) {
+                    current = node->Center[rel];
+                    break;
+                }
+                else if (less_functor()(other, key)) {
+                    current = node->Left[rel];
+                }
+                else {
+                    current = node->Right[rel];
+                }
+            }
+        }
+
+        it->Index = parent;
+        return (NoIndex != parent);
+    }
+
+    bool Contains(const sequence_type& keys) const {
+        Iterator it;
+        if (false == Find_ReturnIfExists(&it, keys))
+            return false;
+
+        index_type rel;
+        node_type* const node = _buckets[it.Bucket].Node(&rel, it.Index);
+
+        return (false != node->HasValue.test(rel));
+    }
+
+    void Clear() {
+        for (Bucket& bucket : _buckets) {
+            for (node_type* node : bucket.Nodes) {
+                allocator_traits::destroy(*this, node);
+                allocator_traits::deallocate(*this, node, 1);
+            }
+
+            bucket.Count = 0;
+            bucket.Nodes.clear_ReleaseMemory();
+        }
+
+        _wordCount = 0;
+    }
+
+private:
+    static index_type BucketIndex_(const sequence_type& keys) {
+        return (hash_functor()(keys.front()) % BucketCount);
+    }
+
+    struct Bucket {
+        size_type Count = 0;
+        node_vector Nodes;
+
+        node_type* Node(index_type* relativeIdx, index_type absoluteIdx) const {
+            Assert(NoIndex != absoluteIdx);
+            Assert(Count > absoluteIdx);
+            Assert(Nodes.size()*BulkSize > absoluteIdx);
+            *relativeIdx = absoluteIdx%_BulkSize;
+            node_type* const node = Nodes[absoluteIdx/_BulkSize];
+            Assert(nullptr != node);
+            return node;
+        }
+    };
+
+    size_type _wordCount;
+
+    Bucket _buckets[BucketCount];
+};
+//----------------------------------------------------------------------------
+//////////////////////////////////////////////////////////////////////////////
+//----------------------------------------------------------------------------
 RobotApp::RobotApp()
 : parent_type(
     L"RobotApp", 100, 100,
@@ -348,21 +620,47 @@ RobotApp::RobotApp()
             }
         }
 
-        STRINGTRIE_SET(Container, CaseSensitive::True, 17) set;
+        VECTOR_THREAD_LOCAL(Container, String) shuffled(words);
+        std::random_shuffle(shuffled.begin(), shuffled.end());
 
-        std::random_shuffle(words.begin(), words.end());
+        {
+            const BenchmarkScope bench("BurstTrie");
 
-        for (const String& word : words)
-            set.Insert_AssertUnique(MakeStringSlice(word));
+            STRINGTRIE_SET(Container, CaseSensitive::True, 13) set;
+            {
+                PROFILING_SCOPE(Global, 4, "BurstTrie construction");
+                for (const String& word : shuffled)
+                    set.Insert_AssertUnique(MakeStringSlice(word));
+            }
 
-        std::random_shuffle(words.begin(), words.end());
+            {
+                PROFILING_SCOPE(Global, 4, "BurstTrie search");
+                forrange(i, 0, 200) {
+                    for (const String& word : words)
+                        if (not set.Contains(MakeStringSlice(word)))
+                            AssertNotReached();
+                }
+            }
+        }
+        {
+            const BenchmarkScope bench("BulkTrie");
 
-        for (const String& word : words)
-            if (not set.Contains(MakeStringSlice(word)))
-                AssertNotReached();
+            BulkTrie<char, void, 8192, 13> set;
+            {
+                PROFILING_SCOPE(Global, 3, "BulkTrie construction");
+                for (const String& word : shuffled)
+                    set.Insert_AssertUnique(MakeStringSlice(word));
+            }
 
-        const auto* const node = set.Find(MakeStringSlice(words.front()));
-        Assert(nullptr != node);
+            {
+                PROFILING_SCOPE(Global, 4, "BulkTrie search");
+                forrange(i, 0, 200) {
+                    for (const String& word : words)
+                        if (not set.Contains(MakeStringSlice(word)))
+                            AssertNotReached();
+                    }
+            }
+        }
     }
     {
         const Filename filename = L"Tmp:/robotapp.bin";
