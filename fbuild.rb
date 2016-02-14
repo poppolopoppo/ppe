a8bc401480473b13fdf305f981f94ab7c0142783
poppolopoppo <frostiebek@gmail.com>
2016-02-14 12:36:32 +0100

    Build: fixed fbuild.rb script and added support for versionning
    Visual Studio paths were not correctly constructed (string quoting after '\').
    _solution_path.bff can be invalidated through a version string present at the top of the file.

--- fbuild.rb
+++ fbuild.rb
@@ -1,35 +1,70 @@
 #!/bin/env ruby
 
-SOLUTION_ROOT = File.dirname(__FILE__)
+VERSION='1.00'
+VERSION_HEADER="; Version = <#{VERSION}>"
+
+SOLUTION_ROOT = File.absolute_path(File.dirname(__FILE__))
 SOLUTION_FBUILDROOT = File.join(SOLUTION_ROOT, 'Build')
 SOLUTION_FBUILDCMD = File.join(SOLUTION_FBUILDROOT, 'FBuild.exe')
 SOLUTION_PATHFILE = File.join(SOLUTION_FBUILDROOT, '_solution_path.bff')
 
 Dir.chdir(SOLUTION_ROOT)
 
-unless File.exist?(SOLUTION_PATHFILE)
-    puts "Creating '#{SOLUTION_PATHFILE}' ..."
+def touch_header(filename)
+    File.open(filename, 'r') do |f|
+        return f.read(VERSION_HEADER.length)
+    end
+end
+
+regen=false
+if not File.readable_real?(SOLUTION_PATHFILE)
+    puts "First run, '#{SOLUTION_PATHFILE}' does not exists."
+    regen = true
+elsif touch_header(SOLUTION_PATHFILE) != VERSION_HEADER
+    puts "Version not matching in '#{SOLUTION_PATHFILE}'."
+    regen = true
+end
+
+if regen
+    puts "Generating '#{SOLUTION_PATHFILE}' for FBuild..."
 
     VS110COMNTOOLS=ENV['VS110COMNTOOLS']
     VS120COMNTOOLS=ENV['VS120COMNTOOLS']
     VS140COMNTOOLS=ENV['VS140COMNTOOLS']
 
+    VS110BIN = File.join(VS110COMNTOOLS, '..', '..', 'VC', 'bin')
+    VS120BIN = File.join(VS120COMNTOOLS, '..', '..', 'VC', 'bin')
+    VS140BIN = File.join(VS140COMNTOOLS, '..', '..', 'VC', 'bin')
+
     vs110cluid = nil
     vs120cluid = nil
     vs140cluid = nil
     '00'.upto('99') do |id|
-        vs110cluid = "10#{id}" if File.exists?("#{VS110COMNTOOLS}..\..\VC\bin\10#{id}\clui.dll")
-        vs120cluid = "10#{id}" if File.exists?("#{VS120COMNTOOLS}..\..\VC\bin\10#{id}\clui.dll")
-        vs140cluid = "10#{id}" if File.exists?("#{VS140COMNTOOLS}..\..\VC\bin\10#{id}\clui.dll")
+        vs110cluid = "10#{id}" if File.exists?(File.join(VS110BIN, "10#{id}", 'clui.dll'))
+        vs120cluid = "10#{id}" if File.exists?(File.join(VS110BIN, "10#{id}", 'clui.dll'))
+        vs140cluid = "10#{id}" if File.exists?(File.join(VS110BIN, "10#{id}", 'clui.dll'))
     end
 
+    vs110cluid = 'VS110 not found' if vs110cluid.nil?
+    vs120cluid = 'VS120 not found' if vs120cluid.nil?
+    vs140cluid = 'VS140 not found' if vs140cluid.nil?
+
     File.open(SOLUTION_PATHFILE, 'w') do |f|
+        f.puts VERSION_HEADER
         f.puts "; Fichier généré par fbuild (#{Time.now})."
+        f.puts
         f.puts ".SolutionPath   = '#{SOLUTION_ROOT}'"
+        f.puts
         f.puts ".VS110CLUID     = '#{vs110cluid}'"
         f.puts ".VS120CLUID     = '#{vs120cluid}'"
         f.puts ".VS140CLUID     = '#{vs140cluid}'"
+        f.puts
+        f.puts "; VS110COMNTOOLS = '#{VS110COMNTOOLS}'"
+        f.puts "; VS120COMNTOOLS = '#{VS120COMNTOOLS}'"
+        f.puts "; VS140COMNTOOLS = '#{VS140COMNTOOLS}'"
     end
+
+    puts "Done writing '#{SOLUTION_PATHFILE}'."
 end
 
 exec(SOLUTION_FBUILDCMD, *ARGV)
