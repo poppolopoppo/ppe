78d97aba76d00c6fbf5e1d5ced81ef917cdce3d8
PopPoLoPoPpo <frostiebek@gmail.com>
2016-02-19 13:15:22 +0100

    Meta: Singleton<> and ThreadLocalSingleton<> can be called of child class without a tag
    can write Singleton<int> or Singleton<MyClass>

--- Source/Core/Meta/Singleton.h
+++ Source/Core/Meta/Singleton.h
@@ -33,9 +33,7 @@ public:
 };
 //----------------------------------------------------------------------------
 template <typename T, typename _Impl>
-SingletonHelpers<T, _Impl>::SingletonHelpers() {
-    Assert(this == _Impl::_gInstancePtr);
-}
+SingletonHelpers<T, _Impl>::SingletonHelpers() {}
 //----------------------------------------------------------------------------
 template <typename T, typename _Impl>
 SingletonHelpers<T, _Impl>::~SingletonHelpers() {
@@ -58,19 +56,20 @@ template <typename T, typename _Impl>
 template <typename... _Args>
 void SingletonHelpers<T, _Impl>::Create(_Args&&... args) {
     AssertRelease(nullptr == _Impl::_gInstancePtr);
-    _Impl::_gInstancePtr = ::new (&_Impl::_gStorage) T(std::forward<_Args>(args)... );
+    _Impl::_gInstancePtr = ::new (_Impl::Storage_()) T(std::forward<_Args>(args)... );
 }
 //----------------------------------------------------------------------------
 template <typename T, typename _Impl>
 void SingletonHelpers<T, _Impl>::Destroy() {
     AssertRelease(nullptr != _Impl::_gInstancePtr);
+    Assert(_Impl::Storage_() == _Impl::_gInstancePtr);
     _Impl::_gInstancePtr->~T();
     _Impl::_gInstancePtr = nullptr;
 }
 //----------------------------------------------------------------------------
 //////////////////////////////////////////////////////////////////////////////
 //----------------------------------------------------------------------------
-template <typename T, typename _Tag>
+template <typename T, typename _Tag = T>
 class Singleton : SingletonHelpers<T, Singleton<T, _Tag> > {
 protected:
     Singleton() {}
@@ -85,19 +84,24 @@ public:
     using parent_type::Destroy;
 
 private:
+    static void* Storage_();
     static T *_gInstancePtr;
-    static typename POD_STORAGE(T) _gStorage;
 };
 //----------------------------------------------------------------------------
 template <typename T, typename _Tag>
-typename POD_STORAGE(T) Singleton<T, _Tag>::_gStorage;
+void* Singleton<T, _Tag>::Storage_() {
+    typedef typename POD_STORAGE(T) pod_type;
+    STATIC_ASSERT(std::is_pod<pod_type>::value);
+    static pod_type _gStorage;
+    return &_gStorage;
+}
 //----------------------------------------------------------------------------
 template <typename T, typename _Tag>
 T *Singleton<T, _Tag>::_gInstancePtr = nullptr;
 //----------------------------------------------------------------------------
 //////////////////////////////////////////////////////////////////////////////
 //----------------------------------------------------------------------------
-template <typename T, typename _Tag>
+template <typename T, typename _Tag = T>
 class ThreadLocalSingleton : SingletonHelpers<T, ThreadLocalSingleton<T, _Tag> > {
 protected:
     ThreadLocalSingleton() {}
@@ -112,12 +116,17 @@ public:
     using parent_type::Destroy;
 
 private:
+    static void* Storage_();
     static THREAD_LOCAL T *_gInstancePtr;
-    static THREAD_LOCAL typename POD_STORAGE(T) _gStorage;
 };
 //----------------------------------------------------------------------------
 template <typename T, typename _Tag>
-THREAD_LOCAL typename POD_STORAGE(T) ThreadLocalSingleton<T, _Tag>::_gStorage;
+void* ThreadLocalSingleton<T, _Tag>::Storage_() {
+    typedef typename POD_STORAGE(T) pod_type;
+    STATIC_ASSERT(std::is_pod<pod_type>::value);
+    static THREAD_LOCAL pod_type _gStorageTLS;
+    return &_gStorageTLS;
+}
 //----------------------------------------------------------------------------
 template <typename T, typename _Tag>
 THREAD_LOCAL T *ThreadLocalSingleton<T, _Tag>::_gInstancePtr = nullptr;
