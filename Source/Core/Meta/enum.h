e97f491ef55b4919c198750a15a972198fc91e62
PopPoLoPoPpo <frostiebek@gmail.com>
2016-03-07 06:28:46 +0100

    Meta: fix enum helpers for 64 bits enums on 32 bits platforms

--- Source/Core/Meta/enum.h
+++ Source/Core/Meta/enum.h
@@ -11,17 +11,23 @@ namespace Meta {
 //----------------------------------------------------------------------------
 template <class T, class = typename std::enable_if< std::is_enum<T>::value >::type >
 inline constexpr bool HasFlag(T value, T flag) {
-    return size_t(flag) == (size_t(value) & size_t(flag));
+    typedef typename std::conditional<(sizeof(T) <= sizeof(size_t)), size_t, u64 >::type uint_type;
+    STATIC_ASSERT(sizeof(uint_type) >= sizeof(T));
+    return uint_type(flag) == (uint_type(value) & uint_type(flag));
 }
 //----------------------------------------------------------------------------
 template <class T, class = typename std::enable_if< std::is_enum<T>::value >::type >
 FORCE_INLINE void SetFlag(T *pvalue, T flag) {
-    *pvalue = (T)(size_t(*pvalue) | size_t(flag));
+    typedef typename std::conditional<(sizeof(T) <= sizeof(size_t)), size_t, u64 >::type uint_type;
+    STATIC_ASSERT(sizeof(uint_type) >= sizeof(T));
+    *pvalue = (T)(uint_type(*pvalue) | uint_type(flag));
 }
 //----------------------------------------------------------------------------
 template <class T, class = typename std::enable_if< std::is_enum<T>::value >::type >
 FORCE_INLINE void UnsetFlag(T *pvalue, T flag) {
-    *pvalue = (T)(size_t(*pvalue) & (~size_t(flag)) );
+    typedef typename std::conditional<(sizeof(T) <= sizeof(size_t)), size_t, u64 >::type uint_type;
+    STATIC_ASSERT(sizeof(uint_type) >= sizeof(T));
+    *pvalue = (T)(uint_type(*pvalue) & (~uint_type(flag)) );
 }
 //----------------------------------------------------------------------------
 //////////////////////////////////////////////////////////////////////////////
