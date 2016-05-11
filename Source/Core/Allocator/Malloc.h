baf43ed3f954cccc5c449095dd9b817073d30330
PopPoLoPoPpo <frostiebek@gmail.com>
2016-05-12 01:57:04 +0200

    Allocator: ALIGNED_ALLOCATED_DEF() macro to override new/delete operators with aligned allocation routines

--- Source/Core/Allocator/Malloc.h
+++ Source/Core/Allocator/Malloc.h
@@ -93,4 +93,23 @@ void* realloc(void *ptr, size_t size, typename std::enable_if< !IsNaturalyAligne
 //----------------------------------------------------------------------------
 //////////////////////////////////////////////////////////////////////////////
 //----------------------------------------------------------------------------
+#define ALIGNED_ALLOCATED_DEF(_Type, _Alignment) \
+public: \
+    void* operator new(size_t size) { \
+        return Core::malloc<_Alignment>(sizeof(_Type)); \
+    } \
+    void* operator new(size_t, void* ptr) { \
+        Assert(ptr); \
+        Likely(ptr); \
+        return ptr; \
+    } \
+    \
+    void operator delete(void* ptr) { \
+        Core::free<_Alignment>(ptr); \
+    } \
+    void operator delete(void* ptr, size_t) { operator delete(ptr); } \
+    void operator delete(void*, void*) {}
+//----------------------------------------------------------------------------
+//////////////////////////////////////////////////////////////////////////////
+//----------------------------------------------------------------------------
 } //!namespace Core
