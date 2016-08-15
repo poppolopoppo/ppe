20f2080b08310c6dd5a553fa7fae1ce50bebe14a
PopPoLoPoPpo <frostiebek@gmail.com>
2016-08-15 23:18:59 +0200

    Core: fix macro expansions for malloc/free()

--- Source/Core/Allocator/TrackingMalloc-inl.h
+++ Source/Core/Allocator/TrackingMalloc-inl.h
@@ -9,36 +9,36 @@ namespace Core {
 template <typename _MemoryDomain>
 inline void* tracking_malloc(size_t size) {
 #ifdef USE_MEMORY_DOMAINS
-    return malloc(_MemoryDomain::TrackingData, size);
+    return (malloc)(_MemoryDomain::TrackingData, size);
 #else
-    return malloc(size);
+    return (malloc)(size);
 #endif
 }
 //----------------------------------------------------------------------------
 template <typename _MemoryDomain>
 inline void tracking_free(void *ptr)  {
 #ifdef USE_MEMORY_DOMAINS
-    free(_MemoryDomain::TrackingData, ptr);
+    (free)(_MemoryDomain::TrackingData, ptr);
 #else
-    free(ptr);
+    (free)(ptr);
 #endif
 }
 //----------------------------------------------------------------------------
 template <typename _MemoryDomain>
 void* tracking_calloc(size_t nmemb, size_t size) {
 #ifdef USE_MEMORY_DOMAINS
-    return calloc(_MemoryDomain::TrackingData, nmemb, size);
+    return (calloc)(_MemoryDomain::TrackingData, nmemb, size);
 #else
-    return calloc(nmemb, size);
+    return (calloc)(nmemb, size);
 #endif
 }
 //----------------------------------------------------------------------------
 template <typename _MemoryDomain>
 void* tracking_realloc(void *ptr, size_t size) {
 #ifdef USE_MEMORY_DOMAINS
-    return realloc(_MemoryDomain::TrackingData, ptr, size);
+    return (realloc)(_MemoryDomain::TrackingData, ptr, size);
 #else
-    return realloc(ptr, size);
+    return (realloc)(ptr, size);
 #endif
 }
 //----------------------------------------------------------------------------
