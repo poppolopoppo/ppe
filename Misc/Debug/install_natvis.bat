b765ebea9b9aeaf91a1f38f59fc3d5a104747071
PopPoLoPoPpo <frostiebek@gmail.com>
2017-02-02 00:37:28 +0100

    Misc: updated natvis installation script for Visual 2017

--- Misc/Debug/install_natvis.bat
+++ Misc/Debug/install_natvis.bat
@@ -1,2 +1,3 @@
 xcopy /Y "%~d0%~p0Core.natvis" "%USERPROFILE%\Documents\Visual Studio 2013\Visualizers\"
 xcopy /Y "%~d0%~p0Core.natvis" "%USERPROFILE%\Documents\Visual Studio 2015\Visualizers\"
+xcopy /Y "%~d0%~p0Core.natvis" "%USERPROFILE%\Documents\Visual Studio 2017\Visualizers\"
