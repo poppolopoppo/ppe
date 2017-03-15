961ea272e9463a9abca6af1d9f8b48c781f0055d
PopPoLoPoPpo <frostiebek@gmail.com>
2017-03-15 22:52:39 +0100

    Container: string hash map

--- Source/Core/Container/StringHashMap.h
+++ Source/Core/Container/StringHashMap.h
@@ -36,8 +36,6 @@ using TBasicStringViewHashMap = THashMap<
 #define WSTRINGVIEW_HASHMAP_THREAD_LOCAL(_DOMAIN, _VALUE, _CASE_SENSITIVE) \
     ::Core::TBasicStringViewHashMap<wchar_t, _VALUE, _CASE_SENSITIVE, THREAD_LOCAL_ALLOCATOR(_DOMAIN, ::Core::TPair<::Core::TBasicStringView<wchar_t> COMMA _VALUE>)>
 //----------------------------------------------------------------------------
-//////////////////////////////////////////////////////////////////////////////
-//----------------------------------------------------------------------------
 #define STRINGVIEW_HASHMAP_MEMOIZE(_DOMAIN, _VALUE, _CASE_SENSITIVE) \
     HASHMAP(_DOMAIN, ::Core::TBasicStringViewHashMemoizer<char COMMA _CASE_SENSITIVE>, _VALUE)
 //----------------------------------------------------------------------------
@@ -49,6 +47,46 @@ using TBasicStringViewHashMap = THashMap<
 //----------------------------------------------------------------------------
 #define WSTRINGVIEW_HASHMAP_MEMOIZE_THREAD_LOCAL(_DOMAIN, _VALUE, _CASE_SENSITIVE) \
     HASHMAP_THREAD_LOCAL(_DOMAIN, ::Core::TBasicStringViewHashMemoizer<wchar_t COMMA _CASE_SENSITIVE>, _VALUE)
+//----------------------------------------------------------------------------
+//////////////////////////////////////////////////////////////////////////////
+//----------------------------------------------------------------------------
+template <
+    typename _Char,
+    typename _Value,
+    ECase _Sensitive,
+    typename _Allocator = ALLOCATOR(Container, TPair<TBasicString<_Char> COMMA _Value>)
+>
+using TBasicStringHashMap = THashMap<
+    TBasicString<_Char>,
+    _Value,
+    TStringHasher<_Char, _Sensitive>,
+    TStringEqualTo<_Char, _Sensitive>,
+    _Allocator
+>;
+//----------------------------------------------------------------------------
+#define STRING_HASHMAP(_DOMAIN, _VALUE, _CASE_SENSITIVE) \
+    ::Core::TBasicStringHashMap<char, _VALUE, _CASE_SENSITIVE, ALLOCATOR(_DOMAIN, ::Core::TPair<::Core::TBasicString<char> COMMA _VALUE>)>
+//----------------------------------------------------------------------------
+#define STRING_HASHMAP_THREAD_LOCAL(_DOMAIN, _VALUE, _CASE_SENSITIVE) \
+    ::Core::TBasicStringHashMap<char, _VALUE, _CASE_SENSITIVE, THREAD_LOCAL_ALLOCATOR(_DOMAIN, ::Core::TPair<::Core::TBasicString<char> COMMA _VALUE>)>
+//----------------------------------------------------------------------------
+#define WSTRING_HASHMAP(_DOMAIN, _VALUE, _CASE_SENSITIVE) \
+    ::Core::TBasicStringHashMap<wchar_t, _VALUE, _CASE_SENSITIVE, ALLOCATOR(_DOMAIN, ::Core::TPair<::Core::TBasicString<wchar_t> COMMA _VALUE>)>
+//----------------------------------------------------------------------------
+#define WSTRING_HASHMAP_THREAD_LOCAL(_DOMAIN, _VALUE, _CASE_SENSITIVE) \
+    ::Core::TBasicStringHashMap<wchar_t, _VALUE, _CASE_SENSITIVE, THREAD_LOCAL_ALLOCATOR(_DOMAIN, ::Core::TPair<::Core::TBasicString<wchar_t> COMMA _VALUE>)>
+//----------------------------------------------------------------------------
+#define STRING_HASHMAP_MEMOIZE(_DOMAIN, _VALUE, _CASE_SENSITIVE) \
+    HASHMAP(_DOMAIN, ::Core::TBasicStringHashMemoizer<char COMMA _CASE_SENSITIVE>, _VALUE)
+//----------------------------------------------------------------------------
+#define STRING_HASHMAP_MEMOIZE_THREAD_LOCAL(_DOMAIN, _VALUE, _CASE_SENSITIVE) \
+    HASHMAP_THREAD_LOCAL(_DOMAIN, ::Core::TBasicStringHashMemoizer<char COMMA _CASE_SENSITIVE>, _VALUE)
+//----------------------------------------------------------------------------
+#define WSTRING_HASHMAP_MEMOIZE(_DOMAIN, _VALUE, _CASE_SENSITIVE) \
+    HASHMAP(_DOMAIN, ::Core::TBasicStringHashMemoizer<wchar_t COMMA _CASE_SENSITIVE>, _VALUE)
+//----------------------------------------------------------------------------
+#define WSTRING_HASHMAP_MEMOIZE_THREAD_LOCAL(_DOMAIN, _VALUE, _CASE_SENSITIVE) \
+    HASHMAP_THREAD_LOCAL(_DOMAIN, ::Core::TBasicStringHashMemoizer<wchar_t COMMA _CASE_SENSITIVE>, _VALUE)
 //----------------------------------------------------------------------------
 //////////////////////////////////////////////////////////////////////////////
 //----------------------------------------------------------------------------
--- Source/Core/IO/String.h
+++ Source/Core/IO/String.h
@@ -151,6 +151,65 @@ std::basic_ostream<_Char, _Traits>& operator <<(
 //----------------------------------------------------------------------------
 //////////////////////////////////////////////////////////////////////////////
 //----------------------------------------------------------------------------
+template <typename _Char, ECase _Sensitive>
+struct TCharEqualTo : public std::binary_function<const _Char, const _Char, bool> {
+    bool operator ()(const _Char& lhs, const _Char& rhs) const { return lhs == rhs; }
+};
+template <typename _Char>
+struct TCharEqualTo<_Char, ECase::Insensitive> : public std::binary_function<const _Char, const _Char, bool> {
+    bool operator ()(const _Char& lhs, const _Char& rhs) const { return ToLower(lhs) == ToLower(rhs); }
+};
+//----------------------------------------------------------------------------
+template <typename _Char, ECase _Sensitive>
+struct TCharLess : public std::binary_function<const _Char, const _Char, bool> {
+    bool operator ()(const _Char& lhs, const _Char& rhs) const { return lhs < rhs; }
+};
+template <typename _Char>
+struct TCharLess<_Char, ECase::Insensitive> : public std::binary_function<const _Char, const _Char, bool> {
+    bool operator ()(const _Char& lhs, const _Char& rhs) const { return ToLower(lhs) < ToLower(rhs); }
+};
+//----------------------------------------------------------------------------
+template <typename _Char, ECase _Sensitive>
+struct TCharCase : public std::unary_function<const _Char, _Char> {
+    _Char operator ()(const _Char& ch) const { return ch; }
+};
+template <typename _Char>
+struct TCharCase<_Char, ECase::Insensitive> : public std::unary_function<const _Char, _Char> {
+    _Char operator ()(const _Char& ch) const { return ToLower(ch); }
+};
+//----------------------------------------------------------------------------
+//////////////////////////////////////////////////////////////////////////////
+//----------------------------------------------------------------------------
+template <typename _Char, ECase _Sensitive>
+struct TStringEqualTo : TStringViewEqualTo <_Char, _Sensitive> {
+    bool operator ()(const TBasicString<_Char>& lhs, const TBasicString<_Char>& rhs) const {
+        return TStringViewEqualTo <_Char, _Sensitive>::operator ()(lhs.MakeView(), rhs.MakeView());
+    }
+};
+//----------------------------------------------------------------------------
+template <typename _Char, ECase _Sensitive>
+struct TStringLess : TStringViewLess <_Char, _Sensitive> {
+    bool operator ()(const TBasicString<_Char>& lhs, const TBasicString<_Char>& rhs) const {
+        return TStringViewLess <_Char, _Sensitive>::operator ()(lhs.MakeView(), rhs.MakeView());
+    }
+};
+//----------------------------------------------------------------------------
+template <typename _Char, ECase _Sensitive>
+struct TStringHasher : TStringViewHasher<_Char, _Sensitive> {
+    size_t operator ()(const TBasicString<_Char>& str) const {
+        return TStringViewHasher<_Char, _Sensitive>::operator ()(str.MakeView());
+    }
+};
+//----------------------------------------------------------------------------
+template <typename _Char, ECase _Sensitive>
+using TBasicStringHashMemoizer = THashMemoizer<
+    TBasicString<_Char>,
+    TStringHasher<_Char, _Sensitive>,
+    TStringEqualTo<_Char, _Sensitive>
+>;
+//----------------------------------------------------------------------------
+//////////////////////////////////////////////////////////////////////////////
+//----------------------------------------------------------------------------
 } //!namespace Core
 
 #include "Core/IO/String-inl.h"
