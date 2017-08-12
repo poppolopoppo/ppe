29e73988005f2e0f3411c1118ebd881fec43f793
PopPoLoPoPpo <frostiebek@gmail.com>
2017-08-12 02:14:54 +0200

    Application: fix tabs

--- Source/Core.Application/ApplicationWindow.h
+++ Source/Core.Application/ApplicationWindow.h
@@ -30,6 +30,10 @@ public:
                         size_t width, size_t height );
     virtual ~FApplicationWindow();
 
+    FGamepadInputHandler& Gamepad() { return *_gamepad; }
+    FKeyboardInputHandler& Keyboard() { return *_keyboard; }
+    FMouseInputHandler& Mouse() { return *_mouse; }
+
     const FGamepadInputHandler& Gamepad() const { return *_gamepad; }
     const FKeyboardInputHandler& Keyboard() const { return *_keyboard; }
     const FMouseInputHandler& Mouse() const { return *_mouse; }
@@ -39,29 +43,29 @@ public:
     virtual void Start() override;
     virtual void Shutdown() override;
 
-	PUBLIC_EVENT(OnGamepadButtonUp,		void, FApplicationWindow* app, const FGamepadState& gamepad, size_t index);
-	PUBLIC_EVENT(OnGamepadButtonPressed,void, FApplicationWindow* app, const FGamepadState& gamepad, size_t index);
-	PUBLIC_EVENT(OnGamepadButtonDown,	void, FApplicationWindow* app, const FGamepadState& gamepad, size_t index);
-	PUBLIC_EVENT(OnGamepadLeftStick,	void, FApplicationWindow* app, const FGamepadState& gamepad, size_t index);
-	PUBLIC_EVENT(OnGamepadRightStick,	void, FApplicationWindow* app, const FGamepadState& gamepad, size_t index);
-	PUBLIC_EVENT(OnGamepadLeftTrigger,	void, FApplicationWindow* app, const FGamepadState& gamepad, size_t index);
-	PUBLIC_EVENT(OnGamepadRightTrigger,	void, FApplicationWindow* app, const FGamepadState& gamepad, size_t index);
-	PUBLIC_EVENT(OnGamepadConnect,		void, FApplicationWindow* app, const FGamepadState& gamepad, size_t index);
-	PUBLIC_EVENT(OnGamepadDisconnect,	void, FApplicationWindow* app, const FGamepadState& gamepad, size_t index);	
+    PUBLIC_EVENT(OnGamepadButtonUp,        void, FApplicationWindow* app, const FGamepadState& gamepad, size_t index);
+    PUBLIC_EVENT(OnGamepadButtonPressed,void, FApplicationWindow* app, const FGamepadState& gamepad, size_t index);
+    PUBLIC_EVENT(OnGamepadButtonDown,    void, FApplicationWindow* app, const FGamepadState& gamepad, size_t index);
+    PUBLIC_EVENT(OnGamepadLeftStick,    void, FApplicationWindow* app, const FGamepadState& gamepad, size_t index);
+    PUBLIC_EVENT(OnGamepadRightStick,    void, FApplicationWindow* app, const FGamepadState& gamepad, size_t index);
+    PUBLIC_EVENT(OnGamepadLeftTrigger,    void, FApplicationWindow* app, const FGamepadState& gamepad, size_t index);
+    PUBLIC_EVENT(OnGamepadRightTrigger,    void, FApplicationWindow* app, const FGamepadState& gamepad, size_t index);
+    PUBLIC_EVENT(OnGamepadConnect,        void, FApplicationWindow* app, const FGamepadState& gamepad, size_t index);
+    PUBLIC_EVENT(OnGamepadDisconnect,    void, FApplicationWindow* app, const FGamepadState& gamepad, size_t index);
 
-	PUBLIC_EVENT(OnKeyboardKeyUp,		void, FApplicationWindow* app, const FKeyboardState& keyboard);
-	PUBLIC_EVENT(OnKeyboardKeyPressed,	void, FApplicationWindow* app, const FKeyboardState& keyboard);
-	PUBLIC_EVENT(OnKeyboardKeyDown,		void, FApplicationWindow* app, const FKeyboardState& keyboard);
+    PUBLIC_EVENT(OnKeyboardKeyUp,        void, FApplicationWindow* app, const FKeyboardState& keyboard);
+    PUBLIC_EVENT(OnKeyboardKeyPressed,    void, FApplicationWindow* app, const FKeyboardState& keyboard);
+    PUBLIC_EVENT(OnKeyboardKeyDown,        void, FApplicationWindow* app, const FKeyboardState& keyboard);
 
-	PUBLIC_EVENT(OnMouseButtonUp,		void, FApplicationWindow* app, const FMouseState& mouse);
-	PUBLIC_EVENT(OnMouseButtonPressed,	void, FApplicationWindow* app, const FMouseState& mouse);
-	PUBLIC_EVENT(OnMouseButtonDown,		void, FApplicationWindow* app, const FMouseState& mouse);
-	PUBLIC_EVENT(OnMouseMove,			void, FApplicationWindow* app, const FMouseState& mouse);
-	PUBLIC_EVENT(OnMouseEnter,			void, FApplicationWindow* app, const FMouseState& mouse);
-	PUBLIC_EVENT(OnMouseLeave,			void, FApplicationWindow* app, const FMouseState& mouse);
+    PUBLIC_EVENT(OnMouseButtonUp,        void, FApplicationWindow* app, const FMouseState& mouse);
+    PUBLIC_EVENT(OnMouseButtonPressed,    void, FApplicationWindow* app, const FMouseState& mouse);
+    PUBLIC_EVENT(OnMouseButtonDown,        void, FApplicationWindow* app, const FMouseState& mouse);
+    PUBLIC_EVENT(OnMouseMove,            void, FApplicationWindow* app, const FMouseState& mouse);
+    PUBLIC_EVENT(OnMouseEnter,            void, FApplicationWindow* app, const FMouseState& mouse);
+    PUBLIC_EVENT(OnMouseLeave,            void, FApplicationWindow* app, const FMouseState& mouse);
 
 protected:
-	virtual void Update_AfterDispatch() override;
+    virtual void Update_AfterDispatch() override;
 
 private:
     TUniquePtr<FGamepadInputHandler> _gamepad;
