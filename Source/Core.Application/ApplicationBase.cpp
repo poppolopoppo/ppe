30717434020573dd1e051222d44eeef16b17dd02
PopPoLoPoPpo <frostiebek@gmail.com>
2017-09-08 01:22:14 +0200

    Application: report memory status at start and at shutdown of app

--- Source/Core.Application/ApplicationBase.cpp
+++ Source/Core.Application/ApplicationBase.cpp
@@ -3,6 +3,7 @@
 #include "ApplicationBase.h"
 
 #include "Core/Diagnostic/Logger.h"
+#include "Core/Memory/MemoryDomain.h"
 
 namespace Core {
 namespace Application {
@@ -16,9 +17,11 @@ FApplicationBase::~FApplicationBase() {}
 //----------------------------------------------------------------------------
 void FApplicationBase::Start() {
     LOG(Info, L"[Application] Start <{0}>", _appname);
+    ReportAllTrackingData();
 }
 //----------------------------------------------------------------------------
 void FApplicationBase::Shutdown() {
+    ReportAllTrackingData();
     LOG(Info, L"[Application] Shutdown <{0}>", _appname);
 }
 //----------------------------------------------------------------------------
