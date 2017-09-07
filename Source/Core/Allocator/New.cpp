694883ce6ca06fa9368bb66cd9326c5dd39f3b97
PopPoLoPoPpo <frostiebek@gmail.com>
2017-09-08 01:21:07 +0200

    Allocator: fix default allocation operators override
    no need for headers, this is overrided at linktime
    https://stackoverflow.com/questions/8186018/how-to-properly-replace-global-new-delete-operators
    
    isolated to its own TU

--- Source/Core/Allocator/New.cpp
+++ Source/Core/Allocator/New.cpp
@@ -1,25 +1,36 @@
 #include "stdafx.h"
 
-#include "New.h"
 #include "Malloc.h"
+
+#define CORE_OVERRIDE_NEW_OPERATORS 1
+
+#ifdef CPP_VISUALSTUDIO
+#   define CORE_RETURN_NOT_NULL     _Check_return_ _Ret_notnull_   _Post_writable_byte_size_(size)
+#   define CORE_RETURN_MAYBE_NULL   _Check_return_ _Ret_maybenull_ _Post_writable_byte_size_(size) _Success_(return != NULL)
+#   define CORE_THROW_BADALLOC      throw()
+#else
+#   define CORE_RETURN_NOT_NULL
+#   define CORE_RETURN_MAYBE_NULL
+#   define CORE_THROW_BADALLOC      throw(std::bad_alloc)
+#endif
 
 #if CORE_OVERRIDE_NEW_OPERATORS
 //----------------------------------------------------------------------------
 //////////////////////////////////////////////////////////////////////////////
 //----------------------------------------------------------------------------
-CORE_RETURN_NOT_NULL void *operator new(size_t size) {
+CORE_RETURN_NOT_NULL void *operator new(size_t size) CORE_THROW_BADALLOC {
     return (Core::malloc)(size);
 }
 //----------------------------------------------------------------------------
-CORE_RETURN_MAYBE_NULL void *operator new(size_t size, std::nothrow_t const& ) throw()  {
+CORE_RETURN_MAYBE_NULL void *operator new(size_t size, std::nothrow_t const& ) noexcept  {
     return (Core::malloc)(size);
 }
 //----------------------------------------------------------------------------
-CORE_RETURN_NOT_NULL void *operator new[](size_t size)  {
+CORE_RETURN_NOT_NULL void *operator new[](size_t size) CORE_THROW_BADALLOC{
     return (Core::malloc)(size);
 }
 //----------------------------------------------------------------------------
-CORE_RETURN_MAYBE_NULL void *operator new[](size_t size,std::nothrow_t const& ) throw() {
+CORE_RETURN_MAYBE_NULL void *operator new[](size_t size,std::nothrow_t const& ) noexcept {
     return (Core::malloc)(size);
 }
 //----------------------------------------------------------------------------
