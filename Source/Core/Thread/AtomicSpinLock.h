e69dc15d1606a429f1c0d4fccd71a0833914285e
PopPoLoPoPpo <frostiebek@gmail.com>
2017-09-20 03:29:28 +0200

    Thread: simpler FAtomicSpinLock without cache line padding (useless with proper atomic ops)

--- Source/Core/Thread/AtomicSpinLock.h
+++ Source/Core/Thread/AtomicSpinLock.h
@@ -14,52 +14,41 @@ namespace Core {
 //----------------------------------------------------------------------------
 // A fast, lightweight spinlock
 //----------------------------------------------------------------------------
-#pragma warning( push )
-#pragma warning( disable : 4324 ) // la structure a été remplie en raison du spécificateur d'alignement.
-CACHELINE_ALIGNED class FAtomicSpinLock {
-public:
-    FAtomicSpinLock() { _state.clear(); }
+struct FAtomicSpinLock {
+    std::atomic_flag State = ATOMIC_FLAG_INIT;
 
-    FAtomicSpinLock(const FAtomicSpinLock& ) = delete;
-    FAtomicSpinLock& operator =(const FAtomicSpinLock& ) = delete;
-
-    void Unlock() { _state.clear(std::memory_order_release); }
-    bool TryLock() { return false == _state.test_and_set(std::memory_order_acquire); }
+    void Unlock() { State.clear(std::memory_order_release); }
+    bool TryLock() { return false == State.test_and_set(std::memory_order_acquire); }
 
     void Lock() {
-        while(_state.test_and_set(std::memory_order_acquire))
+        while(State.test_and_set(std::memory_order_acquire))
             _mm_pause();
     }
 
     struct FScope {
-        FScope(FAtomicSpinLock& lock) : _lock(lock) { _lock.Lock(); }
-        ~FScope() { _lock.Unlock(); }
-
-        FScope(const FScope& ) = delete;
-        FScope& operator =(const FScope& ) = delete;
-
-    private:
-        FAtomicSpinLock& _lock;
+        FAtomicSpinLock& Barrier;
+        FScope(FAtomicSpinLock& barrier) 
+            : Barrier(barrier) { 
+            Barrier.Lock(); 
+        }
+        ~FScope() { 
+            Barrier.Unlock(); 
+        }
     };
 
     struct FTryScope {
-        FTryScope(FAtomicSpinLock& lock) : _lock(lock), _locked(lock.TryLock()) {}
-        ~FTryScope() { if (_locked) _lock.Unlock(); }
-
-        FTryScope(const FTryScope& ) = delete;
-        FTryScope& operator =(const FTryScope& ) = delete;
-
-        bool Locked() const { return _locked; }
-
-    private:
-        FAtomicSpinLock& _lock;
-        bool _locked;
+        FAtomicSpinLock& Barrier;
+        const bool Locked;
+        FTryScope(FAtomicSpinLock& barrier) 
+            : Barrier(barrier)
+            , Locked(barrier.TryLock()) {
+        }
+        ~FTryScope() { 
+            if (Locked)  
+                Barrier.Unlock(); 
+        }
     };
-
-private:
-    std::atomic_flag _state;
 };
-#pragma warning( pop )
 //----------------------------------------------------------------------------
 //////////////////////////////////////////////////////////////////////////////
 //----------------------------------------------------------------------------
