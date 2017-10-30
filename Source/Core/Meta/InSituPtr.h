4115c47f7c552c96122414115f2b153670f55c38
PopPoLoPoPpo <frostiebek@gmail.com>
2017-10-30 23:53:34 +0100

    Meta: refactored TInSituPtr<>

--- Source/Core/Meta/InSituPtr.h
+++ Source/Core/Meta/InSituPtr.h
@@ -13,23 +13,23 @@ namespace Meta {
 template <typename T>
 struct TInSituPtr {
 #ifdef ARCH_X86
-    STATIC_CONST_INTEGRAL(size_t, NullMagick, 0xDEADF001);
+    STATIC_CONST_INTEGRAL(intptr_t, NullMagick, 0xDEADF001ul);
 #else
-    STATIC_CONST_INTEGRAL(size_t, NullMagick, 0xDEADF001DEADF001);
+    STATIC_CONST_INTEGRAL(intptr_t, NullMagick, 0xDEADF001DEADF001ull);
 #endif
     union {
-        size_t Magick = NullMagick;
-        POD_STORAGE(T) Inplace;
+        POD_STORAGE(T) InSitu;
+        intptr_t VTable = NullMagick;
     };
 
-    bool Valid() const { return (Magick != NullMagick); }
-    operator void* () const { return (Valid() ? this : nullptr); }
+    bool Valid() const { return (VTable != NullMagick); }
+    operator const void* () const { return (Valid() ? this : nullptr); }
 
     T* get() {
         Assert(Valid());
-        return reinterpret_cast<T*>(std::addressof(Inplace));
+        return reinterpret_cast<T*>(std::addressof(InSitu));
     }
-    const T* get() const { return const_cast<TInSituPtr>(this)->get(); }
+    const T* get() const { return const_cast<TInSituPtr*>(this)->get(); }
 
     T& operator *() { return *get(); }
     const T& operator *() const { return *get(); }
@@ -38,16 +38,35 @@ struct TInSituPtr {
     const T* operator ->() const { return get(); }
 
     template <typename U, typename... _Args>
-    TEnableIf< std::is_base_of<T, U>::value, U* > Create(_Args&&... args) {
+    U* Create(_Args&&... args) {
+        STATIC_ASSERT(std::is_base_of<T, U>::value);
         STATIC_ASSERT(sizeof(U) == sizeof(T));
         Assert(not Valid());
-        U* const result = new ((void*)std::addressof(Inplace)) U{ std::forward<_Args>(args)... };
+        U* const result = new ((void*)std::addressof(InSitu)) U{ std::forward<_Args>(args)... };
         Assert(Valid());
         return result;
     }
 
     void Destroy() {
+        Assert(Valid());
         get()->~T();
+        VTable = NullMagick
+    }
+
+    template <typename U, typename... _Args>
+    static TInSituPtr Make(_Args&&... args) {
+        TInSituPtr p;
+        p.Create<U>(std::forward<_Args>(args)...);
+        return p;
+    }
+
+    inline friend bool operator ==(const TInSituPtr& lhs, const TInSituPtr& rhs) {
+        STATIC_ASSERT(sizeof(lhs) == sizeof(intptr_t));
+        STATIC_ASSERT(sizeof(rhs) == sizeof(intptr_t));
+        return (lhs.VTable == rhs.VTable);
+    }
+    inline friend bool operator !=(const TInSituPtr& lhs, const TInSituPtr& rhs) {
+        return not operator ==(lhs, rhs);
     }
 };
 //----------------------------------------------------------------------------
