c07972ec58b0645bd3e09255c6e5827ba752fc4d
PopPoLoPoPpo <frostiebek@gmail.com>
2017-10-30 23:48:44 +0100

    Maths: TPrimeNumberProduct<> helper for prime numbers product manipulation
    which has interesting features : if a prime number product is int divible by a prime number then it contains it
    very useful for representing hierarchies with a single id.

--- Source/Core/Maths/PrimeNumbers.h
+++ Source/Core/Maths/PrimeNumbers.h
@@ -13,4 +13,62 @@ CORE_API u16 ClosestPrimeU16Floor(u16 val);
 //----------------------------------------------------------------------------
 //////////////////////////////////////////////////////////////////////////////
 //----------------------------------------------------------------------------
+// Prime number product :
+// Leverage prime numbers factorization properties
+// Enables to test efficiently inheritance for instance
+//----------------------------------------------------------------------------
+template <typename _Tag>
+class TPrimeNumberProduct {
+public:
+    TPrimeNumberProduct() : TPrimeNumberProduct(0) {}
+
+    TPrimeNumberProduct(const TPrimeNumberProduct& other) : TPrimeNumberProduct(other._value) {}
+    TPrimeNumberProduct& operator =(const TPrimeNumberProduct& other) { _value = other._value; return (*this); }
+
+    u64 Value() const { return _value; }
+
+    bool Contains(const TPrimeNumberProduct& parent) const {
+        Assert(_value != 0);
+        Assert(parent._value != 0);
+        return ((_value / parent._value) * parent._value == _value);
+    }
+
+    inline friend void swap(TPrimeNumberProduct& lhs, TPrimeNumberProduct& rhs) {
+        std::swap(lhs._value, rhs._value);
+    }
+
+    inline friend bool operator ==(TPrimeNumberProduct lhs, TPrimeNumberProduct rhs) {
+        return (lhs._value == rhs._value);
+    }
+
+    inline friend bool operator !=(TPrimeNumberProduct lhs, TPrimeNumberProduct rhs) {
+        return (lhs._value != rhs._value);
+    }
+
+    static TPrimeNumberProduct Prime(size_t index) {
+        Assert(index < lengthof(GPrimeNumbersU16));
+        return TPrimeNumberProduct(u64(GPrimeNumbersU16[index]));
+    }
+
+    static TPrimeNumberProduct Combine(TPrimeNumberProduct lhs, TPrimeNumberProduct rhs) {
+        Assert(lhs._value);
+        Assert(rhs._value);
+        return TPrimeNumberProduct(lhs._value * rhs._value);
+    }
+
+private:
+    explicit TPrimeNumberProduct(u64 value) : _value(value) {}
+
+    u64 _value;
+};
+//----------------------------------------------------------------------------
+template <typename _Char, typename _Traits, typename _Tag>
+std::basic_ostream<_Char, _Traits>& operator <<(
+    std::basic_ostream<_Char, _Traits>& oss,
+    const TPrimeNumberProduct<_Tag>& number ) {
+    return oss << number.Value();
+}
+//----------------------------------------------------------------------------
+//////////////////////////////////////////////////////////////////////////////
+//----------------------------------------------------------------------------
 } //!namespace Core
