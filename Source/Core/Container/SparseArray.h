47a8d0d099dabddaf78c6be062740846ac17ad8b
PopPoLoPoPpo <frostiebek@gmail.com>
2017-10-30 23:42:13 +0100

    Container: TSparseArray<> stub

--- Source/Core/Container/SparseArray.h
+++ Source/Core/Container/SparseArray.h
@@ -2,105 +2,14 @@
 
 #include "Core/Core.h"
 
-#include "Core/Allocator/Alloca.h"
-#include "Core/Meta/AlignedStorage.h"
-#include "Core/Memory/MemoryView.h"
 
-#include <type_traits>
-
-// TODO : DataArray<T> http://greysphere.tumblr.com/
+// TODO : DataArray<T> http://greysphere.tumblr.com/post/31601463396/data-arrays
 
 namespace Core {
 //----------------------------------------------------------------------------
 //////////////////////////////////////////////////////////////////////////////
 //----------------------------------------------------------------------------
-#define STACKLOCAL_POD_SPARSEARRAY(T, _NAME, _COUNT) \
-    MALLOCA(Core::TSparseArraySlot<T>, CONCAT(_Alloca_, _NAME), _COUNT); \
-    Core::TPodSparseArray<T> _NAME( CONCAT(_Alloca_, _NAME).MakeView() )
-//----------------------------------------------------------------------------
-#define STACKLOCAL_SPARSEARRAY(T, _NAME, _COUNT) \
-    MALLOCA(Core::TSparseArraySlot<T>, CONCAT(_Alloca_, _NAME), _COUNT); \
-    Core::TSparseArray<T> _NAME( CONCAT(_Alloca_, _NAME).MakeView() )
-//----------------------------------------------------------------------------
-//////////////////////////////////////////////////////////////////////////////
-//----------------------------------------------------------------------------
-template <typename T>
-class TSparseArraySlot {
-public:
-    typedef T value_type;
 
-    typedef TAddPointer<T> pointer;
-    typedef TAddPointer<const T> const_pointer;
-    typedef TAddReference<T> reference;
-    typedef TAddReference<const T> const_reference;
-
-    typedef size_t size_type;
-    typedef ptrdiff_t difference_type;
-
-    typedef size_type FDataID;
-
-    T Element;
-    FDataID Id;
-};
-//----------------------------------------------------------------------------
-template <typename T, bool _IsPod = Meta::TIsPod<T>::value >
-class TSparseArray {
-public:
-    typedef TSparseArraySlot<T> FSlot;
-    typedef typename FSlot::FDataID FDataID;
-
-    typedef typename FSlot::value_type value_type;
-    typedef typename FSlot::pointer pointer;
-    typedef typename FSlot::const_pointer const_pointer;
-    typedef typename FSlot::reference reference;
-    typedef typename FSlot::const_reference const_reference;
-
-    typedef typename FSlot::size_type size_type;
-    typedef typename FSlot::difference_type difference_type;
-
-    TSparseArray();
-    TSparseArray(FSlot* storage, size_type capacity);
-    explicit TSparseArray(const TMemoryView<FSlot>& storage);
-    ~TSparseArray() { clear(); }
-
-    size_type capacity() const { return _capacity; }
-    size_type size() const { return _size; }
-    bool empty() const { return (0 == _size); }
-
-    void clear();
-
-    reference Alloc();
-    void Free(reference elt);
-
-    FDataID GetID(const_reference elt) const;
-
-    reference Get(FDataID id) const;
-    pointer GetIFP(FDataID id) const;
-
-    bool Next(pointer& pelt) const;
-};
-//----------------------------------------------------------------------------
-//////////////////////////////////////////////////////////////////////////////
-//----------------------------------------------------------------------------
-template <typename T>
-using TPodSparseArray = TSparseArray<T, true>;
-//----------------------------------------------------------------------------
-//////////////////////////////////////////////////////////////////////////////
-//----------------------------------------------------------------------------
-template <typename T, size_t _Capacity, size_t _Alignment = std::alignment_of<T>::value >
-class TFixedSizeSparseArray : public TSparseArray<T> {
-public:
-    typedef TSparseArray<T> parent_type;
-
-    using typename parent_type::value_type;
-    using typename parent_type::pointer;
-    using typename parent_type::const_pointer;
-    using typename parent_type::reference;
-    using typename parent_type::const_reference;
-
-    using typename parent_type::size_type;
-    using typename parent_type::difference_type;
-};
 //----------------------------------------------------------------------------
 //////////////////////////////////////////////////////////////////////////////
 //----------------------------------------------------------------------------
