adb523927ee4c38fa6b537fd8fb69ccad1fa245c
PopPoLoPoPpo <frostiebek@gmail.com>
2017-10-30 23:51:33 +0100

    Meta: refactored POD_STORAGE(T) to use a template using (no need to specify typename in templates)

--- Source/Core/Meta/AlignedStorage.h
+++ Source/Core/Meta/AlignedStorage.h
@@ -7,21 +7,12 @@ namespace Meta {
 //----------------------------------------------------------------------------
 //////////////////////////////////////////////////////////////////////////////
 //----------------------------------------------------------------------------
-#define ALIGNED_STORAGE(_SIZE_IN_BYTES, _ALIGNMENT) \
-    std::aligned_storage<(_SIZE_IN_BYTES), (_ALIGNMENT)>::type
+template <size_t _SizeInBytes, size_t _Alignement>
+using TAlignedStorage = typename std::aligned_storage<_SizeInBytes, _Alignement>::type;
+//----------------------------------------------------------------------------
+#define ALIGNED_STORAGE(_SIZE_IN_BYTES, _ALIGNMENT) Core::Meta::TAlignedStorage<_SIZE_IN_BYTES, _ALIGNMENT>
 //----------------------------------------------------------------------------
 #define POD_STORAGE(T) ALIGNED_STORAGE(sizeof(COMMA_PROTECT(T)), alignof(COMMA_PROTECT(T)))
-//----------------------------------------------------------------------------
-//////////////////////////////////////////////////////////////////////////////
-//----------------------------------------------------------------------------
-// gave up using on those, compilers will crash or fail in very strange ways...
-/*
-template <size_t _SizeInBytes, size_t _Alignement>
-using AlignedStorage = typename std::aligned_storage<_SizeInBytes, _Alignement>::type;
-//----------------------------------------------------------------------------
-template <typename T, size_t _Alignment = std::alignment_of<T>::value >
-using PODStorage = AlignedStorage<sizeof(T), _Alignment>;
-*/
 //----------------------------------------------------------------------------
 //////////////////////////////////////////////////////////////////////////////
 //----------------------------------------------------------------------------
