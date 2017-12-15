becbf96356d143e619e381e310e30979944048c5
PopPoLoPoPpo <frostiebek@gmail.com>
2017-12-15 01:22:46 +0100

    Diagnostic: FCurrentProcess knows current build configuration and compilation time

--- Source/Core/Diagnostic/CurrentProcess.cpp
+++ Source/Core/Diagnostic/CurrentProcess.cpp
@@ -47,10 +47,11 @@ FCurrentProcess::FCurrentProcess(void *applicationHandle, int nShowCmd, const wc
 #ifdef USE_DEBUG_LOGGER
     LOG(Info, L"[Process] Started '{0}' with {1} parameters.", _fileName, _args.size());
     LOG(Info, L"[Process] Directory = '{0}'.", _directory);
+    LOG(Info, L"[Process] Build configuration = " WIDESTRING(STRINGIZE(BUILDCONFIG)));
+    LOG(Info, L"[Process] Compiled at = " WIDESTRING(__DATE__) L"  " WIDESTRING(__TIME__));
     LOG(Info, L"[Process] Application Handle = '{0}', nShowCmd = '{1}'.", _applicationHandle, _nShowCmd);
-    for (size_t i = 0; i < _args.size(); ++i) {
+    forrange(i, 0, _args.size())
         LOG(Info, L"- [{0:2}] '{1}'", i, _args[i]);
-    }
 #endif
 }
 //----------------------------------------------------------------------------
