d5eb3c9307dcf2ae8bbb2420a18ab80a8a238fc9
PopPoLoPoPpo <frostiebek@gmail.com>
2017-12-26 01:22:15 +0100

    Meta: replaced std::function entirely with custom Meta::TFunction<>
    - guaranteed to never allocate (in situ storage)
    - can be assigned from a free function
    - can be assigned from a member function, const or not
    - can be assigned from a compatible lambda
    - will calls destructor for captured variables IFN

--- Source/Core/Meta/Function.h
+++ Source/Core/Meta/Function.h
@@ -1,148 +1,221 @@
 #pragma once
 
-#include "Core/Allocator/Allocation.h"
+#include "Core/Meta/AlignedStorage.h"
+#include "Core/Meta/TypeTraits.h"
 
 namespace Core {
 namespace Meta {
 //----------------------------------------------------------------------------
 //////////////////////////////////////////////////////////////////////////////
 //----------------------------------------------------------------------------
-#if 0
+// TFunction<> is guaranteed to never allocate, unlike std::function<>
+//----------------------------------------------------------------------------
 template <typename T>
 class TFunction;
 template <typename _Ret, typename... _Args>
-class TFunction<_Ret(_Args...)> : FFunctionAllocator {
+class TFunction<_Ret(_Args...)> {
 public:
-    TFunction() = default;
+    typedef _Ret (*func_type)(_Args...);
 
-    template <typename _Fx>
-    TFunction(const _Fx& other) { assign(other); }
+    template <typename T>
+    static constexpr bool is_callable_v = std::bool_constant<
+            decltype(is_callable_<T>(0))::value &&
+            // Forbid wrapping TFunction in another TFunction :
+            not std::is_same_v<Meta::TDecay<T>, TFunction>
+        >::value;
 
-    template <typename _Fx>
-    TFunction& operator =(const _Fx& other) { assign(other); return (*this); }
+    TFunction() : _func(nullptr) {}
 
-    bool valid() const { return (_storage != storage_type{}); }
-    operator bool() const { return valid(); }
-
-    _Ret operator()(Args&& ...args) const {
-        if (!valid())
-            CORE_THROW_IT(std::bad_function_call());
-
-        return pimpl_()->Invoke(std::forward<Args>(args)...);
+    TFunction(const TFunction& other) { assign_copy_(other); }
+    TFunction& operator =(const TFunction& other) {
+        Reset();
+        assign_copy_(other);
+        return (*this);
     }
 
-    void assign(const TFunction& other) {
-        reset();
-        if (other.valid())
-            other.pimpl_()->Clone(_storage.data());
+    TFunction(TFunction&& rvalue) : TFunction() {
+        Swap(rvalue);
+    }
+    TFunction& operator =(TFunction&& rvalue) {
+        Reset();
+        Swap(rvalue);
+        return (*this);
     }
 
-    void assign(TFunction&& rvalue) {
-        reset();
-        if (other.valid())
-            other.pimpl_()->Clone(_storage.data());
+    TFunction(func_type fn)
+        : _func(fn) {
+        Assert(not is_wrapped_());
     }
 
-    void assign(_Ret(*f)(_Args...)) {
-        make_pimpl_(f);
+    template <typename _Class>
+    TFunction(_Class* obj, _Ret(_Class::*member)(_Args...)) {
+        Assert(obj);
+        Assert(member);
+        struct member_t {
+            _Class* Obj;
+            _Ret(_Class::*Member)(_Args...);
+            _Ret operator ()(_Args... args) const {
+                return (Obj->*Member)(std::forward<_Args>(args)...);
+            }
+        };
+        assign_wrapped_(member_t{ obj, member });
+        Assert(is_pod_());
     }
 
-    void reset() {
-        if (not valid()) return;
-        pimpl_()->~IPimpl_();
-        _storage = storage_type{};
-        Assert(not valid());
+    template <typename _Class>
+    TFunction(const _Class* obj, _Ret(_Class::*member)(_Args...) const) {
+        Assert(obj);
+        Assert(member);
+        struct member_const_t {
+            const _Class* Obj;
+            _Ret(_Class::*Member)(_Args...) const;
+            _Ret operator ()(_Args... args) const {
+                return (Obj->*Member)(std::forward<_Args>(args)...);
+            }
+        };
+        assign_wrapped_(member_const_t{ obj, member });
+        Assert(is_pod_());
     }
 
-    void swap(TFunction& other) {
-        std::swap(_storage, other._storage);
+    template <typename T, typename = Meta::TEnableIf<is_callable_v<T>> >
+    TFunction(T&& lambda) {
+        assign_wrapped_(std::move(lambda));
     }
+
+    ~TFunction() {
+        if (is_destructible_())
+            ((IWrapper_*)&_inSitu)->~IWrapper_();
+    }
+
+    CORE_FAKEBOOL_OPERATOR_DECL() { return _func; }
+    bool Valid() const { return (nullptr != _func); }
+
+    _Ret operator ()(_Args... args) const {
+        return Invoke(std::forward<_Args>(args)...);
+    }
+
+    _Ret Invoke(_Args... args) const {
+        Assert(_func);
+        return ((is_wrapped_())
+            ? ((wrapper_type)(_wrapper&~GMaskAll))(&_inSitu, std::forward<_Args>(args)...)
+            : _func(std::forward<_Args>(args)...));
+    }
+
+    void Reset() {
+        if (is_destructible_())
+            ((IWrapper_*)&_inSitu)->~IWrapper_();
+
+        _func = nullptr;
+    }
+
+    bool Equals(const TFunction& other) const {
+        return (_func == _func && _inSitu == other._inSitu);
+    }
+
+    void Swap(TFunction& other) {
+        std::swap(_func, other._func);
+        std::swap(_inSitu, other._inSitu);
+    }
+
+    inline friend bool operator ==(const TFunction& lhs, const TFunction& rhs) { return (lhs.Equals(rhs)); }
+    inline friend bool operator !=(const TFunction& lhs, const TFunction& rhs) { return (not operator ==(lhs, rhs)); }
+
+    inline friend void swap(TFunction& lhs, TFunction& rhs) { lhs.Swap(rhs); }
 
 private:
-    typedef std::array<intptr_t, 4> storage_type;
-    alignas(intptr_t) storage_type _storage;
+    static constexpr intptr_t GMaskPOD = (intptr_t(1) << CODE3264(30, 62));
+    static constexpr intptr_t GMaskWrapped = (intptr_t(1) << CODE3264(31, 63));
+    static constexpr intptr_t GMaskAll = (GMaskPOD | GMaskWrapped);
 
-    struct IPimpl_ {
-        virtual ~IPimpl_() = default;
-        virtual _Ret Invoke(Args&& ...args) const = 0;
-        virtual void Copy(void* dst) const = 0;
+    typedef _Ret(*wrapper_type)(const void*, _Args...);
+
+    union {
+        func_type _func;
+        intptr_t _wrapper;
+    };
+    STATIC_ASSERT(sizeof(intptr_t) == sizeof(func_type));
+    STATIC_ASSERT(sizeof(intptr_t) == sizeof(wrapper_type));
+
+    bool is_pod_() const { return (0 != (_wrapper&GMaskPOD)); }
+    bool is_wrapped_() const { return (0 != (_wrapper&GMaskWrapped)); }
+    bool is_destructible_() const { return (GMaskWrapped == (_wrapper&GMaskAll)); }
+
+    static constexpr size_t GInSituSize = (32 - sizeof(intptr_t));
+    ALIGNED_STORAGE(GInSituSize, 1) _inSitu;
+
+    template <typename T, typename _R = decltype(std::declval<T>()(std::declval<_Args>()...)) >
+    static typename std::is_same<_Ret, _R>::type is_callable_(int);
+    template <typename T>
+    static std::false_type is_callable_(...);
+
+    struct IWrapper_ {
+        virtual ~IWrapper_() {}
+        virtual void CopyTo(void* dst) const = 0;
     };
 
-    template <typename _Fx>
-    struct TPimpl_ : IPimpl_<_Fx> {
-        _Fx _fx;
-        TPimpl_(const _Fx& value) : _fx(value) {}
-        TPimpl_(_Fx&& rvalue) : _fx(std::move(rvalue)) {}
-        virtual _Ret Invoke(Args&& ...args) const override { return _fx(std::forward<_Args>(args)...); }
-        virtual void Copy(void* dst) const override { new (dst) TPimpl_<_Fx>(_fx); }
+    template <typename T>
+    struct TWrapper_ : IWrapper_ {
+        T Data;
+        TWrapper_(T&& data) : Data(std::move(data)) {}
+        virtual void CopyTo(void* dst) const override {
+            new (dst) TWrapper_<T>(T(Data));
+        }
     };
 
-    template <typename _Fx>
-    TPimpl_<_Fx>* make_pimpl_(const _Fx& fx) {
-        if (valid())
-            pimpl_()->~IPimpl_();
-        return new (_storage.data()) TPimpl_<_Fx>(fx);
+    template <typename _Data>
+    void assign_wrapped_(_Data&& data) {
+        assign_wrapped_(std::move(data), std::is_trivially_destructible<_Data>::type{});
     }
 
-    IPimpl_* pimpl_() { return reinterpret_cast<IPimpl_*>(_storage.data()); }
-    const IPimpl_* pimpl_() const { return reinterpret_cast<const IPimpl_*>(_storage.data()); }
+    template <typename _Data>
+    void assign_wrapped_(_Data&& data, std::true_type) {
+        using wrapped_type = _Data;
+        STATIC_ASSERT(sizeof(wrapped_type) <= GInSituSize);
+        new (&_inSitu) wrapped_type(std::move(data));
+        const wrapper_type w = [](const void* inSitu, _Args... args) -> _Ret {
+            return (*(wrapped_type*)inSitu)(std::forward<_Args>(args)...);
+        };
+        Assert(0 == (intptr_t(w) & GMaskAll));
+        _wrapper = (intptr_t(w) | GMaskAll);
+    }
+
+    template <typename _Data>
+    void assign_wrapped_(_Data&& data, std::false_type) {
+        using wrapped_type = TWrapper_<_Data>;
+        STATIC_ASSERT(sizeof(wrapped_type) <= GInSituSize);
+        new (&_inSitu) wrapped_type(std::move(data));
+        const wrapper_type w = [](const void* inSitu, _Args... args) -> _Ret {
+            return ((wrapped_type*)inSitu)->Data(std::forward<_Args>(args)...);
+        };
+        Assert(0 == (intptr_t(w) & GMaskAll));
+        _wrapper = (intptr_t(w) | GMaskWrapped);
+    }
+
+    void assign_copy_(const TFunction& other) {
+        _func = other._func;
+        if (_func && is_wrapped_()) {
+            if (is_destructible_())
+                ((const IWrapper_*)&other._inSitu)->CopyTo(&_inSitu);
+            else
+                ::memcpy(&_inSitu, &other._inSitu, sizeof(_inSitu));
+        }
+    }
 };
-#else
-template <typename T, typename _Allocator = ALLOCATOR(Functional, char)>
-class TFunction : _Allocator, public std::function<T> {
-public:
-    typedef _Allocator allocator_type;
-    typedef std::function<T> parent_type;
-
-    using parent_type::target;
-    using parent_type::target_type;
-    using parent_type::operator bool;
-
-    TFunction() = default;
-
-    TFunction(const parent_type& other)
-        : parent_type(std::allocator_arg, allocator_(), other)
-    {}
-
-    TFunction& operator =(const parent_type& other) {
-        parent_type::assign(other, allocator_());
-        return (*this);
-    }
-
-    TFunction(parent_type&& rvalue)
-        : parent_type(std::allocator_arg, allocator_(), std::move(rvalue))
-    {}
-
-    TFunction& operator =(parent_type&& rvalue) {
-        parent_type::assign(std::move(rvalue), allocator_())
-        return (*this);
-    }
-
-    template <typename _Fx>
-    TFunction(_Fx fx)
-        : parent_type(std::allocator_arg, allocator_(), std::forward<_Fx>(fx))
-    {}
-
-    template <typename _Fx>
-    TFunction& operator =(_Fx fx) {
-        parent_type::assign(std::forward<_Fx>(fx), allocator_());
-        return (*this);
-    }
-
-    template<class _Fx>
-    void assign(_Fx&& fx) {
-        parent_type::assign(std::forward<_Fx>(fx), allocator_());
-    }
-
-    void swap(TFunction& other) {
-        swap(allocator_(), other.allocator_());
-        parent_type::swap(other);
-    }
-
-private:
-    allocator_type& allocator_() { return static_cast<allocator_type&>(*this); }
-};
-#endif
+//----------------------------------------------------------------------------
+template <typename _Ret, typename... _Args>
+TFunction<_Ret(_Args...)> MakeFunction(_Ret(*func)(_Args...)) {
+    return TFunction<_Ret(_Args...)>(func);
+}
+//----------------------------------------------------------------------------
+template <typename _Ret, typename _Class, typename... _Args>
+TFunction<_Ret(_Args...)> MakeFunction(_Class* obj, _Ret(_Class::*member)(_Args...)) {
+    return TFunction<_Ret(_Args...)>(obj, member);
+}
+//----------------------------------------------------------------------------
+template <typename _Ret, typename _Class, typename... _Args>
+TFunction<_Ret(_Args...)> MakeFunction(const _Class* obj, _Ret(_Class::*member)(_Args...) const) {
+    return TFunction<_Ret(_Args...)>(obj, member);
+}
 //----------------------------------------------------------------------------
 //////////////////////////////////////////////////////////////////////////////
 //----------------------------------------------------------------------------
