44e878830c47be05996a3e63a8f0782319e255f4
PopPoLoPoPpo <frostiebek@gmail.com>
2018-02-06 13:33:01 +0100

    MEta: clean checked_cast<>()+ numeric limits

--- Source/Core/Meta/Cast.h
+++ Source/Core/Meta/Cast.h
@@ -1,37 +1,48 @@
 #pragma once
 
+#include "Core/Meta/Assert.h"
 #include "Core/Meta/TypeHash.h"
+#include "Core/Meta/TypeTraits.h"
 
 #include <type_traits>
+
+#ifdef WITH_CORE_ASSERT
+#   define USE_CORE_CHECKEDCAST 1
+#else
+#   define USE_CORE_CHECKEDCAST 0
+#endif
 
 namespace Core {
 //----------------------------------------------------------------------------
 //////////////////////////////////////////////////////////////////////////////
 //----------------------------------------------------------------------------
+template <typename _Dst>
+constexpr FORCE_INLINE NOALIAS _Dst checked_cast(_Dst id) noexcept {
+    return id;
+}
+//----------------------------------------------------------------------------
 template <typename _Dst, typename _Src>
-inline NOALIAS typename std::enable_if<
+NOALIAS Meta::TEnableIf<
     std::is_arithmetic<_Dst>::value &&
     std::is_arithmetic<_Src>::value &&
     !std::is_same<_Dst, _Src>::value,
-    _Dst>::type
+    _Dst >
     checked_cast(const _Src value) noexcept {
-#ifdef _DEBUG
+#if USE_CORE_CHECKEDCAST
     const _Dst result = static_cast<_Dst>(value);
-
     Assert(static_cast<_Src>(result) == value);
     return result;
 #else
     return static_cast<_Dst>(value);
 #endif
 }
-//----------------------------------------------------------------------------
 template <typename _Dst, typename _Src>
-inline NOALIAS typename std::enable_if<
+NOALIAS Meta::TEnableIf<
     (std::is_pointer<_Dst>::value ^ std::is_pointer<_Src>::value) &&
     !std::is_same<_Dst, _Src>::value,
-    _Dst>::type
+    _Dst >
     checked_cast(const _Src value) noexcept {
-#ifdef _DEBUG
+#if USE_CORE_CHECKEDCAST
     const _Dst result = reinterpret_cast<_Dst>(value);
     Assert(reinterpret_cast<_Src>(result) == value);
     return result;
@@ -39,14 +50,13 @@ inline NOALIAS typename std::enable_if<
     return reinterpret_cast<_Dst>(value);
 #endif
 }
-//----------------------------------------------------------------------------
 template <typename _Dst, typename _Src>
-inline NOALIAS typename std::enable_if<
+NOALIAS Meta::TEnableIf<
     (std::is_pointer<_Dst>::value && std::is_pointer<_Src>::value) &&
     !std::is_same<_Dst, _Src>::value,
-    _Dst>::type
+    _Dst >
     checked_cast(_Src value) {
-#ifdef _DEBUG
+#if USE_CORE_CHECKEDCAST
     if (nullptr == value)
         return nullptr;
     const _Dst result = static_cast<_Dst>(value);
@@ -57,10 +67,7 @@ inline NOALIAS typename std::enable_if<
 #endif
 }
 //----------------------------------------------------------------------------
-template <typename _Dst>
-FORCE_INLINE NOALIAS const _Dst& checked_cast(const _Dst& value) noexcept {
-    return value;
-}
+//////////////////////////////////////////////////////////////////////////////
 //----------------------------------------------------------------------------
 template <typename T>
 FORCE_INLINE NOALIAS T& remove_const(const T& value) {
