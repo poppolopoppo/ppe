ed0ba7c4c19fe664023cb0d998872910429d24b7
PopPoLoPoPpo <frostiebek@gmail.com>
2018-05-07 23:59:21 +0200

    Thread: new FAtomicOrderedLock atomic lock which keep thread orders when releasing contention

--- Source/Core/Thread/AtomicSpinLock.h
+++ Source/Core/Thread/AtomicSpinLock.h
@@ -14,9 +14,10 @@ namespace Core {
 //----------------------------------------------------------------------------
 // A fast, lightweight spinlock
 //----------------------------------------------------------------------------
-struct FAtomicSpinLock {
+class FAtomicSpinLock {
     std::atomic_flag State = ATOMIC_FLAG_INIT;
 
+public:
     void Unlock() { State.clear(std::memory_order_release); }
     bool TryLock() { return false == State.test_and_set(std::memory_order_acquire); }
 
@@ -27,25 +28,64 @@ struct FAtomicSpinLock {
 
     struct FScope {
         FAtomicSpinLock& Barrier;
-        FScope(FAtomicSpinLock& barrier) 
-            : Barrier(barrier) { 
-            Barrier.Lock(); 
+        FScope(FAtomicSpinLock& barrier)
+            : Barrier(barrier) {
+            Barrier.Lock();
         }
-        ~FScope() { 
-            Barrier.Unlock(); 
+        ~FScope() {
+            Barrier.Unlock();
         }
     };
 
     struct FTryScope {
         FAtomicSpinLock& Barrier;
         const bool Locked;
-        FTryScope(FAtomicSpinLock& barrier) 
+        FTryScope(FAtomicSpinLock& barrier)
             : Barrier(barrier)
             , Locked(barrier.TryLock()) {
         }
-        ~FTryScope() { 
-            if (Locked)  
-                Barrier.Unlock(); 
+        ~FTryScope() {
+            if (Locked)
+                Barrier.Unlock();
+        }
+    };
+};
+//----------------------------------------------------------------------------
+//////////////////////////////////////////////////////////////////////////////
+//----------------------------------------------------------------------------
+// Less fast, but order preserving
+//----------------------------------------------------------------------------
+class FAtomicOrderedLock {
+    std::atomic<size_t> Locked = 0;
+    std::atomic<size_t> Revision = size_t(-1); // <-- must be == to (Locked - 1)
+
+public:
+    struct FScope {
+        FAtomicOrderedLock& Barrier;
+#ifdef WITH_CORE_ASSERT
+        const size_t Order;
+#endif
+
+        FScope(FAtomicOrderedLock& barrier)
+            : Barrier(barrier)
+#ifdef WITH_CORE_ASSERT
+            , Order(++Barrier.Revision)
+        {
+#else
+        {
+            const size_t Order = ++Barrier.Revision;
+#endif
+            for (;;) { // spin for lock
+                size_t revision = Order;
+                if (Barrier.Locked.compare_exchange_weak(revision, revision, std::memory_order_acquire))
+                    return;
+            }
+        }
+
+        ~FScope() {
+            Assert_NoAssume(Barrier.Locked == Order);
+
+            ++Barrier.Locked; // release the locks, the next waiting thread will stop spinning
         }
     };
 };
