b7cd3d256e9a740b9b3869f1719e1c5143026774
PopPoLoPoPpo <phamtom.x@gmail.com>
2018-09-05 02:44:16 +0200

    TestApp: added FPlatformNotication samples in FTestApp::Start/Shutdown()

--- Source/Tools/UnitTest/Private/TestApp.cpp
+++ Source/Tools/UnitTest/Private/TestApp.cpp
@@ -2,6 +2,11 @@
 
 #include "TestApp.h"
 
+#include "Diagnostic/CurrentProcess.h"
+#include "HAL/PlatformCrash.h"
+#include "HAL/PlatformDebug.h"
+#include "HAL/PlatformNotification.h"
+
 namespace PPE {
 namespace Test {
 //----------------------------------------------------------------------------
@@ -29,20 +34,103 @@ FTestApp::~FTestApp() {}
 void FTestApp::Start() {
     parent_type::Start();
 
+    using FAppNotify = Application::FPlatformNotification;
+
+    FAppNotify::ShowSystray();
+
+    FAppNotify::AddSystrayCommand(
+        L"Debug",
+        L"Create mini dump",
+        []() {
+        FPlatformCrash::WriteMiniDump();
+    });
+    FAppNotify::AddSystrayCommand(
+        L"Debug",
+        L"Debug break",
+        []() {
+        PPE_DEBUG_BREAK();
+    });
+
+#if USE_PPE_PLATFORM_DEBUG
+    FAppNotify::AddSystrayCommand(
+        L"Memory",
+        L"Check memory",
+        []() {
+        FPlatformDebug::CheckMemory();
+    });
+#endif
+#if !USE_PPE_FINAL_RELEASE
+    FAppNotify::AddSystrayCommand(
+        L"Memory",
+        L"Dump memory leaks",
+        []() {
+        DumpMemoryLeaks();
+    });
+#endif
+    FAppNotify::AddSystrayCommand(
+        L"Memory",
+        L"Report all tracking data",
+        []() {
+        ReportAllTrackingData();
+    });
+
+    FAppNotify::AddSystrayCommand(
+        L"Process",
+        L"Dump process infos",
+        []() {
+        FCurrentProcess::Get().DumpProcessInfos();
+    });
+    FAppNotify::AddSystrayCommand(
+        L"Process",
+        L"Dump process memory",
+        []() {
+        FCurrentProcess::Get().DumpMemoryStats();
+    });
+
+    FAppNotify::NotifySystray(
+        FAppNotify::ENotificationIcon::Info,
+        L"Starting TestApp",
+        L"Unit testing will start with " WSTRINGIZE(BUILDCONFIG) L" build config" );
+
+    constexpr size_t total = 9;
+
+    FAppNotify::SetTaskbarProgress(0, total);
     Test_Allocators();
+    FAppNotify::SetTaskbarProgress(1, total);
     Test_Containers();
+    FAppNotify::SetTaskbarProgress(2, total);
     Test_Format();
+    FAppNotify::SetTaskbarProgress(3, total);
     Test_Thread();
+    FAppNotify::SetTaskbarProgress(4, total);
     Test_VFS();
+    FAppNotify::SetTaskbarProgress(5, total);
     Test_RTTI();
+    FAppNotify::SetTaskbarProgress(6, total);
     Test_XML();
+    FAppNotify::SetTaskbarProgress(7, total);
     Test_Lattice();
+    FAppNotify::SetTaskbarProgress(8, total);
     //Test_Pixmap();  #TODO refactoring the asset generation
     Test_Network();
+    FAppNotify::SetTaskbarProgress(9, total);
+
+    FAppNotify::SetTaskbarState(FAppNotify::ETaskbarState::Indeterminate);
 }
 //----------------------------------------------------------------------------
 void FTestApp::Shutdown() {
     parent_type::Shutdown();
+
+    using FAppNotify = Application::FPlatformNotification;
+
+    FAppNotify::SetTaskbarState(FAppNotify::ETaskbarState::Normal);
+
+    FAppNotify::NotifySystray(
+        FAppNotify::ENotificationIcon::Warning,
+        L"Shutting down TestApp",
+        L"Oh yeah");
+
+    FAppNotify::HideSystray();
 }
 //----------------------------------------------------------------------------
 //////////////////////////////////////////////////////////////////////////////
