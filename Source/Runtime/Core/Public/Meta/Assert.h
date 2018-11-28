229f7d663d2637f6756e8109fb4d6f1a542c0a39
PopPoLoPoPpo <phamtom.x@gmail.com>
2018-11-28 22:39:44 +0100

    Meta: changed AssertXXXMessage() to use __VA_ARGS__
    no comma expansion problem that way

--- Source/Runtime/Core/Public/Meta/Assert.h
+++ Source/Runtime/Core/Public/Meta/Assert.h
@@ -44,12 +44,12 @@ private:
 PPE_CORE_API void AssertionFailed(const wchar_t* msg, const wchar_t *file, unsigned line);
 PPE_CORE_API void SetAssertionHandler(FAssertHandler handler);
 
-#   define AssertMessage(_Expression, _Message) \
-    (void)( (!!(_Expression)) || (PPE::AssertionFailed(_Message, WIDESTRING(__FILE__), __LINE__), 0) )
+#   define AssertMessage(_Message, ...) \
+    (void)( (!!(__VA_ARGS__)) || (PPE::AssertionFailed(_Message, WIDESTRING(__FILE__), __LINE__), 0) )
 
-#   define AssertMessage_NoAssume(_Expression, _Message) AssertMessage(_Expression, _Message)
+#   define AssertMessage_NoAssume(_Message, ...) AssertMessage(_Message, COMMA_PROTECT(__VA_ARGS__))
 
-#   define Verify(...) AssertMessage(COMMA_PROTECT(__VA_ARGS__), WIDESTRING(#__VA_ARGS__))
+#   define Verify(...) AssertMessage(WIDESTRING(#__VA_ARGS__), COMMA_PROTECT(__VA_ARGS__))
 
 //----------------------------------------------------------------------------
 #else
@@ -58,12 +58,12 @@ inline void AssertionFailed(const wchar_t *, const wchar_t *, unsigned ) {}
 inline void SetAssertionHandler(FAssertHandler ) {}
 
 #   if WITH_PPE_ASSERT_FALLBACK_TO_ASSUME
-#       define AssertMessage(_Expression, _Message) Assume(_Expression)
+#       define AssertMessage(_Message, ...) Assume(__VA_ARGS__)
 #   else
-#       define AssertMessage(_Expression, _Message) NOOP()
+#       define AssertMessage(_Message, ...) NOOP()
 #   endif
 
-#   define AssertMessage_NoAssume(_Expression, _Message) NOOP()
+#   define AssertMessage_NoAssume(_Message, ...) NOOP()
 
 #   define Verify(...) (void)(__VA_ARGS__)
 
@@ -74,8 +74,8 @@ inline void SetAssertionHandler(FAssertHandler ) {}
 //----------------------------------------------------------------------------
 } //!namespace PPE
 
-#define Assert(...) AssertMessage(COMMA_PROTECT(__VA_ARGS__), WSTRINGIZE(__VA_ARGS__))
-#define Assert_NoAssume(...) AssertMessage_NoAssume(COMMA_PROTECT(__VA_ARGS__), WSTRINGIZE(__VA_ARGS__))
+#define Assert(...) AssertMessage(WSTRINGIZE(__VA_ARGS__), COMMA_PROTECT(__VA_ARGS__))
+#define Assert_NoAssume(...) AssertMessage_NoAssume(WSTRINGIZE(__VA_ARGS__), COMMA_PROTECT(__VA_ARGS__))
 
 namespace PPE {
 //----------------------------------------------------------------------------
@@ -102,15 +102,15 @@ PPE_CORE_API void AssertionReleaseFailed(const wchar_t* msg, const wchar_t *file
 inline void NORETURN AssertionReleaseFailed_NoReturn(const wchar_t* msg, const wchar_t *file, unsigned line) { AssertionReleaseFailed(msg, file, line); }
 PPE_CORE_API void SetAssertionReleaseHandler(FAssertReleaseHandler handler);
 
-#   define AssertReleaseMessage(_Expression, _Message) \
-    (void)( (!!(_Expression)) || (PPE::AssertionReleaseFailed(_Message, WIDESTRING(__FILE__), __LINE__), 0) )
+#   define AssertReleaseMessage(_Message, ...) \
+    (void)( (!!(__VA_ARGS__)) || (PPE::AssertionReleaseFailed(_Message, WIDESTRING(__FILE__), __LINE__), 0) )
 
 #   define AssertReleaseFailed(_Message) \
     PPE::AssertionReleaseFailed_NoReturn(_Message, WIDESTRING(__FILE__), __LINE__)
 
-#   define AssertReleaseMessage_NoAssume(_Expression, _Message) AssertReleaseMessage(_Expression, _Message)
+#   define AssertReleaseMessage_NoAssume(_Message, ...) AssertReleaseMessage(_Message, COMMA_PROTECT(__VA_ARGS__))
 
-#   define VerifyRelease(...) AssertReleaseMessage(COMMA_PROTECT(__VA_ARGS__), WIDESTRING(#__VA_ARGS__))
+#   define VerifyRelease(...) AssertReleaseMessage(WIDESTRING(#__VA_ARGS__), COMMA_PROTECT(__VA_ARGS__))
 
 //----------------------------------------------------------------------------
 #else
@@ -120,12 +120,12 @@ inline void NORETURN AssertionReleaseFailed_NoReturn(const wchar_t*, const wchar
 inline void SetAssertionReleaseHandler(FAssertReleaseHandler ) {}
 
 #   if WITH_PPE_ASSERT_RELEASE_FALLBACK_TO_ASSUME
-#       define AssertReleaseMessage(_Expression, _Message)  Assume(_Expression)
+#       define AssertReleaseMessage(_Message, ...)  Assume(__VA_ARGS__)
 #   else
-#       define AssertReleaseMessage(_Expression, _Message)  NOOP()
+#       define AssertReleaseMessage(_Message, ...)  NOOP()
 #   endif
 
-#   define AssertReleaseMessage_NoAssume(_Expression, _Message)  NOOP()
+#   define AssertReleaseMessage_NoAssume(_Message, ...)  NOOP()
 #   define AssertReleaseFailed(_Message) PPE::AssertionReleaseFailed_NoReturn(nullptr, nullptr, 0)
 
 #   define VerifyRelease(...) (void)(__VA_ARGS__)
@@ -137,8 +137,8 @@ inline void SetAssertionReleaseHandler(FAssertReleaseHandler ) {}
 //----------------------------------------------------------------------------
 } //!namespace PPE
 
-#define AssertRelease(...) AssertReleaseMessage(COMMA_PROTECT(__VA_ARGS__), WSTRINGIZE(__VA_ARGS__))
-#define AssertRelease_NoAssume(...) AssertReleaseMessage_NoAssume(COMMA_PROTECT(__VA_ARGS__), WSTRINGIZE(__VA_ARGS__))
+#define AssertRelease(...) AssertReleaseMessage(WSTRINGIZE(__VA_ARGS__), COMMA_PROTECT(__VA_ARGS__))
+#define AssertRelease_NoAssume(...) AssertReleaseMessage_NoAssume(WSTRINGIZE(__VA_ARGS__), COMMA_PROTECT(__VA_ARGS__))
 
 #define AssertNotReached() AssertReleaseFailed(L"unreachable state")
 #define AssertNotImplemented() AssertReleaseFailed(L"not implemented")
