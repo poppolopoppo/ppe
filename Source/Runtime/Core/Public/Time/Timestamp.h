7c1155276bdd20de02cfab18278aba0304b26fa2
PopPoLoPoPpo <phamtom.x@gmail.com>
2019-12-02 01:02:59 +0100

    Time: FTimestamp manipulation helpers

--- Source/Runtime/Core/Public/Time/Timestamp.h
+++ Source/Runtime/Core/Public/Time/Timestamp.h
@@ -22,6 +22,24 @@ public:
     value_type Value() const { return _value; }
     void SetValue(value_type value) { _value = value; }
 
+    FTimestamp SecondsAgo(value_type seconds) const {
+        Assert_NoAssume(_value > seconds);
+        return FTimestamp{ _value - seconds };
+    }
+    FTimestamp SecondsLater(value_type seconds) const {
+        Assert_NoAssume(INT64_MAX - seconds >= _value);
+        return FTimestamp{ _value + seconds };
+    }
+
+    FTimestamp MinutesAgo(value_type minutes) const { return SecondsAgo(minutes * 60); }
+    FTimestamp MinutesLater(value_type minutes) const { return SecondsLater(minutes * 60); }
+
+    FTimestamp HoursAgo(value_type hours) const { return SecondsAgo(hours * 3600); }
+    FTimestamp HoursLater(value_type hours) const { return SecondsLater(hours * 3600); }
+
+    FTimestamp DaysAgo(value_type days) const { return SecondsAgo(days * 3600 * 24); }
+    FTimestamp DaysLater(value_type days) const { return SecondsLater(days * 3600 * 24); }
+
     FDateTime ToDateTime() const;
     FDateTime ToDateTimeUTC() const;
 
