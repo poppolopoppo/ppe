2325cca32de2d20c4024029513742deffc654ad2
PopPoLoPoPpo <phamtom.x@gmail.com>
2020-01-13 00:40:49 +0100

    Git: fix gitignore for BuildLog.h/cpp (and them)

---
+++ Source/ContentPipeline/BuildGraph/Private/BuildLog.cpp
@@ -0,0 +1,112 @@
+﻿#include "stdafx.h"
+
+#include "BuildLog.h"
+
+#include "BuildEnums.h"
+#include "BuildGraph.h" // LOGGER_CATEGORY()
+#include "BuildNode.h"
+
+#include "Diagnostic/Logger.h"
+#include "IO/BufferedStream.h"
+#include "VirtualFileSystem_fwd.h"
+
+#include <mutex>
+
+namespace PPE {
+namespace ContentPipeline {
+//----------------------------------------------------------------------------
+//////////////////////////////////////////////////////////////////////////////
+//----------------------------------------------------------------------------
+namespace {
+//----------------------------------------------------------------------------
+class FDiagnosticBuildLog : public IBuildLog {
+public:
+    virtual void NodeBegin(const FBuildNode& node) override final {
+        UNUSED(node);
+        LOG(BuildGraph, Emphasis, L" >> {0}", node.RTTI_PathName());
+    }
+
+    virtual void NodeEnd(const FBuildNode& node, EBuildResult result) override {
+        UNUSED(node);
+        switch (result) {
+        case PPE::ContentPipeline::EBuildResult::Unbuilt:
+            LOG(BuildGraph, Debug, L" ---- {0}", node.RTTI_PathName());
+            break;
+        case PPE::ContentPipeline::EBuildResult::UpToDate:
+            LOG(BuildGraph, Info, L" ---> {0}", node.RTTI_PathName());
+            break;
+        case PPE::ContentPipeline::EBuildResult::Built:
+            LOG(BuildGraph, Emphasis, L" [OK] {0}", node.RTTI_PathName());
+            break;
+        case PPE::ContentPipeline::EBuildResult::Failed:
+            LOG(BuildGraph, Error, L" !KO! {0}", node.RTTI_PathName());
+            break;
+        default:
+            AssertNotImplemented();
+        }
+    }
+
+    virtual void TraceDebug(const FWStringView& text) override final {
+        UNUSED(text);
+        LOG_DIRECT(BuildGraph, Debug, text);
+    }
+    virtual void TraceError(const FWStringView& text) override final {
+        UNUSED(text);
+        LOG_DIRECT(BuildGraph, Error, text);
+    }
+    virtual void TraceInfo(const FWStringView& text) override final {
+        UNUSED(text);
+        LOG_DIRECT(BuildGraph, Info, text);
+    }
+    virtual void TraceVerbose(const FWStringView& text) override final {
+        UNUSED(text);
+        LOG_DIRECT(BuildGraph, Verbose, text);
+    }
+    virtual void TraceWarning(const FWStringView& text) override final {
+        UNUSED(text);
+        LOG_DIRECT(BuildGraph, Warning, text);
+    }
+
+    virtual void TraceDebugArgs(const FWStringView& fmt, const FWFormatArgList& args) override final {
+        UNUSED(fmt);
+        UNUSED(args);
+        LOG_ARGS(BuildGraph, Debug, fmt, args);
+    }
+    virtual void TraceErrorArgs(const FWStringView& fmt, const FWFormatArgList& args) override final {
+        UNUSED(fmt);
+        UNUSED(args);
+        LOG_ARGS(BuildGraph, Error, fmt, args);
+    }
+    virtual void TraceInfoArgs(const FWStringView& fmt, const FWFormatArgList& args) override final {
+        UNUSED(fmt);
+        UNUSED(args);
+        LOG_ARGS(BuildGraph, Info, fmt, args);
+    }
+    virtual void TraceVerboseArgs(const FWStringView& fmt, const FWFormatArgList& args) override final {
+        UNUSED(fmt);
+        UNUSED(args);
+        LOG_ARGS(BuildGraph, Verbose, fmt, args);
+    }
+    virtual void TraceWarningArgs(const FWStringView& fmt, const FWFormatArgList& args) override final {
+        UNUSED(fmt);
+        UNUSED(args);
+        LOG_ARGS(BuildGraph, Warning, fmt, args);
+    }
+
+    void Flush() override {
+        FLUSH_LOG();
+    }
+};
+//----------------------------------------------------------------------------
+} //!namespace
+//----------------------------------------------------------------------------
+//////////////////////////////////////////////////////////////////////////////
+//----------------------------------------------------------------------------
+UBuildLog MakeDiagnosticsBuildLog() {
+    return UBuildLog{ new FDiagnosticBuildLog() };
+}
+//----------------------------------------------------------------------------
+//////////////////////////////////////////////////////////////////////////////
+//----------------------------------------------------------------------------
+} //!namespace ContentPipeline
+} //!namespace PPE
---
+++ Source/ContentPipeline/BuildGraph/Public/BuildLog.h
@@ -0,0 +1,81 @@
+﻿#pragma once
+
+#include "BuildGraph_fwd.h"
+
+#include "IO/FileSystem_fwd.h"
+#include "IO/Format.h"
+#include "IO/String_fwd.h"
+#include "Memory/UniquePtr.h"
+
+namespace PPE {
+namespace ContentPipeline {
+//----------------------------------------------------------------------------
+//////////////////////////////////////////////////////////////////////////////
+//----------------------------------------------------------------------------
+FWD_INTEFARCE_UNIQUEPTR(BuildLog);
+//----------------------------------------------------------------------------
+class IBuildLog {
+public:
+    virtual ~IBuildLog() = default;
+
+    virtual void NodeBegin(const FBuildNode& node) = 0;
+    virtual void NodeEnd(const FBuildNode& node, EBuildResult result) = 0;
+
+    virtual void TraceDebug(const FWStringView& text) = 0;
+    virtual void TraceError(const FWStringView& text) = 0;
+    virtual void TraceInfo(const FWStringView& text) = 0;
+    virtual void TraceVerbose(const FWStringView& text) = 0;
+    virtual void TraceWarning(const FWStringView& text) = 0;
+
+    virtual void TraceDebugArgs(const FWStringView& fmt, const FWFormatArgList& args) = 0;
+    virtual void TraceErrorArgs(const FWStringView& fmt, const FWFormatArgList& args) = 0;
+    virtual void TraceInfoArgs(const FWStringView& fmt, const FWFormatArgList& args) = 0;
+    virtual void TraceVerboseArgs(const FWStringView& fmt, const FWFormatArgList& args) = 0;
+    virtual void TraceWarningArgs(const FWStringView& fmt, const FWFormatArgList& args) = 0;
+
+    virtual void Flush() = 0;
+
+public: // Trace helpers:
+    template <typename _Arg0, typename... _Args>
+    void TraceDebug(const FWStringView& fmt, _Arg0&& arg0, _Args&&... args) {
+        TraceLevel_<&IBuildLog::TraceDebugArgs>(fmt, std::forward<_Arg0>(arg0), std::forward<_Args>(args)...);
+    }
+    template <typename _Arg0, typename... _Args>
+    void TraceError(const FWStringView& fmt, _Arg0&& arg0, _Args&&... args) {
+        TraceLevel_<&IBuildLog::TraceErrorArgs>(fmt, std::forward<_Arg0>(arg0), std::forward<_Args>(args)...);
+    }
+    template <typename _Arg0, typename... _Args>
+    void TraceInfo(const FWStringView& fmt, _Arg0&& arg0, _Args&&... args) {
+        TraceLevel_<&IBuildLog::TraceInfoArgs>(fmt, std::forward<_Arg0>(arg0), std::forward<_Args>(args)...);
+    }
+    template <typename _Arg0, typename... _Args>
+    void TraceVerbose(const FWStringView& fmt, _Arg0&& arg0, _Args&&... args) {
+        TraceLevel_<&IBuildLog::TraceVerboseArgs>(fmt, std::forward<_Arg0>(arg0), std::forward<_Args>(args)...);
+    }
+    template <typename _Arg0, typename... _Args>
+    void TraceWarning(const FWStringView& fmt, _Arg0&& arg0, _Args&&... args) {
+        TraceLevel_<&IBuildLog::TraceWarningArgs>(fmt, std::forward<_Arg0>(arg0), std::forward<_Args>(args)...);
+    }
+
+private:
+    using trace_args_f = void (IBuildLog::*)(const FWStringView & fmt, const FWFormatArgList & args);
+
+    template <trace_args_f _TraceArgs, typename _Arg0, typename... _Args >
+    void TraceLevel_(const FWStringView& fmt, _Arg0&& arg0, _Args&&... args) {
+        // args are always passed by pointer, wrapped in a void *
+        // this avoids unintended copies and de-correlates from actual types (_FormatArgs is defined in Format.cpp)
+        typedef details::FWFormatFunctor_ formatfunc_type;
+        const formatfunc_type functors[] = {
+            formatfunc_type::Make(std::forward<_Arg0>(arg0)),
+            formatfunc_type::Make(std::forward<_Args>(args))...
+        };
+        (this->*_TraceArgs)(fmt, MakeView(functors));
+    }
+};
+//----------------------------------------------------------------------------
+PPE_BUILDGRAPH_API UBuildLog MakeDiagnosticsBuildLog();
+//----------------------------------------------------------------------------
+//////////////////////////////////////////////////////////////////////////////
+//----------------------------------------------------------------------------
+} //!namespace ContentPipeline
+} //!namespace PPE
