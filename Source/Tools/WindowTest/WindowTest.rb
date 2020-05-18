326fde462bdc57d69258643090da7d1bb31ab65b
PopPoLoPoPpo <phamtom.x@gmail.com>
2020-05-18 02:55:57 +0200

    Tools: add missing private dependencies from WindowTest
    need to add private dependencies... not ideal

--- Source/Tools/WindowTest/WindowTest.rb
+++ Source/Tools/WindowTest/WindowTest.rb
@@ -1,6 +1,12 @@
 # frozen_string_literal: true
 
 $Build.ppe_executable!(:WindowTest, :Tools) do
+    private_deps!(*namespace[:External]{[
+        double_conversion,
+        farmhash,
+        iaca,
+        lz4,
+        xxHash ]})
     public_deps!(*namespace[:Runtime]{[
         Core(),
         VFS(),
