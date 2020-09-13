a424cf4ec7262eef9292a490c20afd6e94c2c62d
PoPpOloPopPo <phamtom.x@gmail.com>
2020-09-13 20:03:01 +0200

    Build: add site information for failed assertions
    Much easier to locate source problem when looking at generic error
    messages

--- Build/Utils/Log.rb
+++ Build/Utils/Log.rb
@@ -203,15 +203,15 @@ module Build
 
         def self.check(&block)
             if $DEBUG
-                Log.fatal 'assertion failed' unless block.call
+                Log.fatal 'assertion failed at %s', caller_locations(1,1)[0] unless block.call
             end
         end
 
-        def self.expect(x, klass) Log.fatal 'unexpected value: <%s> "%s", need <%s>', x.class, x.to_s, klass unless x.is_a?(klass) end
-        def self.expect?(x, klass) Log.fatal 'unexpected value: <%s> "%s", need <%s>', x.class, x.to_s, klass unless x.nil? || x.is_a?(klass) end
-        def self.unexpected(x) Log.fatal 'unexpected value: <%s> "%s"', x.class, x.to_s end
-        def self.unreached() Log.fatal 'unreachable state' end
-        def self.not_implemented() Log.fatal 'not implemented' end
+        def self.expect(x, klass) Log.fatal 'unexpected value: <%s> "%s", need <%s> at %s', x.class, x, klass, caller_locations(1,1)[0] unless x.is_a?(klass) end
+        def self.expect?(x, klass) Log.fatal 'unexpected value: <%s> "%s", need <%s> at %s', x.class, x, klass, caller_locations(1,1)[0] unless x.nil? || x.is_a?(klass) end
+        def self.unexpected(x) Log.fatal 'unexpected value: <%s> "%s" at %s', x.class, x, caller_locations(1,1)[0] end
+        def self.unreached() Log.fatal 'unreachable state at %s', caller_locations(1,1)[0] end
+        def self.not_implemented() Log.fatal 'not implemented at %s', caller_locations(1,1)[0] end
 
     end #~ Assert
 
