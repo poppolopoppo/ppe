601dd0ebb9c4f6efabd94bd850c09d01088f5b33
PopPopOloPpoPo <phamtom.x@gmail.com>
2020-12-17 16:57:15 +0100

    Allocator: fix SSE impl of FAllocatorBinning::IndexFromSize()
    also used signed intrinsics, which are faster than unsigned ones (_mm_sub vs _mm_subs).

--- Source/Runtime/Core/Public/Allocator/AllocatorBinning.h
+++ Source/Runtime/Core/Public/Allocator/AllocatorBinning.h
@@ -17,7 +17,7 @@ struct FAllocatorBinning {
         480,    544,    624,    704,    800,    928,    1056,   1216,
         1424,   1616,   1840,   2112,   2384,   2720,   3184,   3680,
         4240,   4912,   5696,   6432,   7360,   8480,   9552,   10912,
-        12736,  15008,  16960,  19104,  21824,  24560,  27632,  UINT16_MAX/* padding/dummy */
+        12736,  15008,  16960,  19104,  21824,  24560,  27632,  INT16_MAX/* padding/dummy */
     };
     static CONSTEXPR const u32 MaxBinSize = BinSizes[NumBins - 1];
 
@@ -32,37 +32,24 @@ struct FAllocatorBinning {
         return IndexFromSize(checked_cast<u16>(blockSize));
     }
     static u32 IndexFromSize(u16 blockSize) NOEXCEPT {
-#ifdef __clang__ // codegen out-performs the explicit SSE version
+#ifdef __clang__ // codegen should out-perform the explicit SSE version bellow
         return IndexFromSizeConst(blockSize);
 
 #else
         m128i_t key = ::_mm_set1_epi16(blockSize);
+
         m128i_t cnt = ::_mm_setzero_si128();
-
-        m128i_t sz0 = ::_mm_cmplt_epi16(::_mm_load_si128((m128i_t*)&BinSizes[ 0]), key);
-        cnt = ::_mm_sub_epi16(cnt, sz0);
-
-        m128i_t sz1 = ::_mm_cmplt_epi16(::_mm_load_si128((m128i_t*)&BinSizes[ 8]), key);
-        cnt = ::_mm_sub_epi16(cnt, sz1);
-
-        m128i_t sz2 = ::_mm_cmplt_epi16(::_mm_load_si128((m128i_t*)&BinSizes[16]), key);
-        cnt = ::_mm_sub_epi16(cnt, sz2);
-
-        m128i_t sz3 = ::_mm_cmplt_epi16(::_mm_load_si128((m128i_t*)&BinSizes[24]), key);
-        cnt = ::_mm_sub_epi16(cnt, sz3);
-
-        m128i_t sz4 = ::_mm_cmplt_epi16(::_mm_load_si128((m128i_t*)&BinSizes[32]), key);
-        cnt = ::_mm_sub_epi16(cnt, sz4);
-
-        m128i_t sz5 = ::_mm_cmplt_epi16(::_mm_load_si128((m128i_t*)&BinSizes[40]), key);
-        cnt = ::_mm_sub_epi16(cnt, sz5);
-
-        STATIC_ASSERT(lengthof(BinSizes) == 48);
+        cnt = ::_mm_subs_epi16(cnt, ::_mm_cmplt_epi16(::_mm_load_si128((m128i_t*)&BinSizes[ 0]), key));
+        cnt = ::_mm_subs_epi16(cnt, ::_mm_cmplt_epi16(::_mm_load_si128((m128i_t*)&BinSizes[ 8]), key));
+        cnt = ::_mm_subs_epi16(cnt, ::_mm_cmplt_epi16(::_mm_load_si128((m128i_t*)&BinSizes[16]), key));
+        cnt = ::_mm_subs_epi16(cnt, ::_mm_cmplt_epi16(::_mm_load_si128((m128i_t*)&BinSizes[24]), key));
+        cnt = ::_mm_subs_epi16(cnt, ::_mm_cmplt_epi16(::_mm_load_si128((m128i_t*)&BinSizes[32]), key));
+        cnt = ::_mm_subs_epi16(cnt, ::_mm_cmplt_epi16(::_mm_load_si128((m128i_t*)&BinSizes[40]), key));
 
         cnt = ::_mm_sad_epu8(cnt, ::_mm_setzero_si128());
-        cnt = ::_mm_add_epi16(cnt, _mm_shuffle_epi32(cnt, _MM_SHUFFLE(1, 0, 3, 2)));
+        cnt = ::_mm_adds_epi16(cnt, _mm_shuffle_epi32(cnt, _MM_SHUFFLE(1, 0, 3, 2)));
 
-        return (_mm_cvtsi128_si32(cnt) & 0xFFFFul);
+        return (::_mm_cvtsi128_si32(cnt) & 0xFFFFu);
 
 #endif
     }
