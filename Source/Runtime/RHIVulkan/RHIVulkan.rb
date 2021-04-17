6e08939948c3aaa8d0629b8e7cf298b72f516e53
PopPopOloPpoPo <phamtom.x@gmail.com>
2021-04-17 04:33:37 +0200

    RHIVulkan: depends on Application
    so it can implement IRHIService

--- Source/Runtime/RHIVulkan/RHIVulkan.rb
+++ Source/Runtime/RHIVulkan/RHIVulkan.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
 $Build.ppe_module!(:RHIVulkan) do
-    public_deps!(namespace.Core, namespace.RHI)
+    public_deps!(namespace.Core, namespace.RHI, namespace.Application)
     private_deps!(*namespace[:External]{[vulkan]})
 end
--- Source/Runtime/Runtime.rb
+++ Source/Runtime/Runtime.rb
@@ -4,10 +4,10 @@ $Build.namespace(:Runtime) do
     include!('Core/Core.rb')
     include!('VFS/VFS.rb')
     include!('RHI/RHI.rb')
-    include!('RHIVulkan/RHIVulkan.rb')
     include!('RTTI/RTTI.rb')
     include!('Serialize/Serialize.rb')
     include!('Network/Network.rb')
     include!('Application/Application.rb')
+    include!('RHIVulkan/RHIVulkan.rb')
     include!('Remoting/Remoting.rb')
 end
