39eb984b9c30a2c0fcba8bc3c7885103bbc89fd4
PopPopOloPpoPo <phamtom.x@gmail.com>
2021-06-06 16:21:35 +0200

    Meta: better enum flags helpers with concept validation
    notably an issue with EnumIsFlags() signature made the feature not working

--- Source/Runtime/Core/Public/Meta/Enum.h
+++ Source/Runtime/Core/Public/Meta/Enum.h
@@ -22,37 +22,42 @@ CONSTEXPR auto EnumOrd(_Enum value) {
     return TEnumOrd<_Enum>(value);
 }
 //----------------------------------------------------------------------------
-template <typename _Enum, class = TEnableIf<std::is_enum_v<_Enum>> >
+template <typename _Enum, class = TEnableIf<enum_is_flags_v<_Enum>> >
+CONSTEXPR _Enum EnumOneComplement(_Enum value) {
+    return _Enum(~EnumOrd(value));
+}
+//----------------------------------------------------------------------------
+template <typename _Enum, class = TEnableIf<enum_is_flags_v<_Enum>> >
 CONSTEXPR _Enum EnumAnd(_Enum lhs, _Enum rhs) {
     return _Enum(EnumOrd(lhs) & EnumOrd(rhs));
 }
 //----------------------------------------------------------------------------
-template <typename _Enum, class = TEnableIf<std::is_enum_v<_Enum>> >
+template <typename _Enum, class = TEnableIf<enum_is_flags_v<_Enum>> >
 CONSTEXPR _Enum EnumOr(_Enum lhs, _Enum rhs) {
     return _Enum(EnumOrd(lhs) | EnumOrd(rhs));
 }
 //----------------------------------------------------------------------------
-template <typename _Enum, class = TEnableIf<std::is_enum_v<_Enum>> >
+template <typename _Enum, class = TEnableIf<enum_is_flags_v<_Enum>> >
 CONSTEXPR _Enum EnumAdd(_Enum lhs, _Enum rhs) {
     return _Enum(EnumOrd(lhs) | EnumOrd(rhs));
 }
 //----------------------------------------------------------------------------
-template <typename _Enum, class = TEnableIf<std::is_enum_v<_Enum>> >
+template <typename _Enum, class = TEnableIf<enum_is_flags_v<_Enum>> >
 CONSTEXPR _Enum EnumRemove(_Enum lhs, _Enum rhs) {
     return _Enum(EnumOrd(lhs) & ~EnumOrd(rhs));
 }
 //----------------------------------------------------------------------------
-template <typename _Enum, class = TEnableIf<std::is_enum_v<_Enum>> >
+template <typename _Enum, class = TEnableIf<enum_is_flags_v<_Enum>> >
 CONSTEXPR bool EnumXor(_Enum lhs, _Enum rhs) {
     return !!(EnumOrd(lhs) & EnumOrd(rhs));
 }
 //----------------------------------------------------------------------------
-template <typename _Enum, class = TEnableIf<std::is_enum_v<_Enum>> >
+template <typename _Enum, class = TEnableIf<enum_is_flags_v<_Enum>> >
 CONSTEXPR bool EnumHas(_Enum lhs, TEnumOrd<_Enum> rhs) {
     return ((EnumOrd(lhs) & rhs) == rhs);
 }
 //----------------------------------------------------------------------------
-template <typename _Enum, class = TEnableIf<std::is_enum_v<_Enum>> >
+template <typename _Enum, class = TEnableIf<enum_is_flags_v<_Enum>> >
 CONSTEXPR bool EnumHas(_Enum lhs, _Enum rhs) {
     return EnumHas(lhs, EnumOrd(rhs));
 }
@@ -63,9 +68,10 @@ CONSTEXPR bool EnumLess(_Enum lhs, _Enum rhs) {
 }
 //----------------------------------------------------------------------------
 #define _ENUM_FLAGS_IMPL(_PREFIX, _ENUMTYPE) \
-    CONSTEXPR bool EnumIsFlags(_ENUMTYPE) { return true; } \
+    _PREFIX CONSTEXPR bool EnumIsFlags(_ENUMTYPE) { return true; } \
     MAYBE_UNUSED _PREFIX CONSTEXPR _ENUMTYPE    BitAnd(_ENUMTYPE lhs, _ENUMTYPE rhs)      { return ::PPE::Meta::EnumAnd(lhs, rhs); } \
     MAYBE_UNUSED _PREFIX CONSTEXPR _ENUMTYPE    BitOr(_ENUMTYPE lhs, _ENUMTYPE rhs)       { return ::PPE::Meta::EnumOr(lhs, rhs); } \
+    MAYBE_UNUSED _PREFIX CONSTEXPR _ENUMTYPE    operator ~(_ENUMTYPE value)               { return ::PPE::Meta::EnumOneComplement(value); } \
     MAYBE_UNUSED _PREFIX CONSTEXPR _ENUMTYPE    operator |(_ENUMTYPE lhs, _ENUMTYPE rhs)  { return ::PPE::Meta::EnumOr(lhs, rhs); } \
     MAYBE_UNUSED _PREFIX CONSTEXPR _ENUMTYPE    operator +(_ENUMTYPE lhs, _ENUMTYPE rhs)  { return ::PPE::Meta::EnumAdd(lhs, rhs); } \
     MAYBE_UNUSED _PREFIX CONSTEXPR _ENUMTYPE    operator -(_ENUMTYPE lhs, _ENUMTYPE rhs)  { return ::PPE::Meta::EnumRemove(lhs, rhs); } \
