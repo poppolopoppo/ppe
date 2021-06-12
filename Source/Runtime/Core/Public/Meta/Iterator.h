29c0762cd4a915f34a8b2b33730491b65067de3f
PopPopOloPpoPo <phamtom.x@gmail.com>
2021-06-12 03:35:32 +0200

    Meta: new helpers CopyTo/UnitializedCopyTo() for TIterable<>

--- Source/Runtime/Core/Public/Meta/Iterator.h
+++ Source/Runtime/Core/Public/Meta/Iterator.h
@@ -521,6 +521,15 @@ public:
         return TIterable<std::reverse_iterator<_It>>{ std::make_reverse_iterator(_end), std::make_reverse_iterator(_begin) };
     }
 
+    template <typename _Outp>
+    CONSTEXPR auto CopyTo(_Outp dst) {
+        return std::copy(begin(), end(), dst);
+    }
+    template <typename _Outp>
+    CONSTEXPR auto UnitializedCopyTo(_Outp dst) {
+        return std::uninitialized_copy(begin(), end(), dst);
+    }
+
     template <typename _Pred = Meta::TEqualTo<value_type> >
     CONSTEXPR bool Equals(const TIterable& other, _Pred pred = _Pred{}) const NOEXCEPT {
         return std::equal(_begin, _end, other._begin, other._end, pred);
