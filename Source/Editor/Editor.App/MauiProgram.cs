d2debcfff4e1881847cfee9e972110686386a116
PopPopOloPpoPo <phamtom.x@gmail.com>
2021-10-14 12:50:28 +0200

    Editor: new prototyp editor using NSwag, MAUI, Blazor and MudBlazor
    wow I am entering the dangerous web 2.0 bullshit zone
    more seriously the tech is interesting, but clearly not stable/user-friendly :p
    MudBlazor seems great though, and after some iterations the generated C#
    client for OAS is finally making sense: it's satisfying to see the engine
    talk to this completely foreign environment thanks to a litle custom http
    server !

---
+++ Source/Editor/Editor.App/MauiProgram.cs
@@ -0,0 +1,27 @@
+﻿using Editor.App.Data;
+using Microsoft.AspNetCore.Components.WebView.Maui;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Maui;
+using Microsoft.Maui.Controls.Compatibility;
+using Microsoft.Maui.Controls.Hosting;
+using Microsoft.Maui.Hosting;
+using MudBlazor.Services;
+
+namespace Editor.App
+{
+    public static class MauiProgram
+    {
+        public static MauiApp CreateMauiApp()
+        {
+            var builder = MauiApp.CreateBuilder();
+            builder
+                .RegisterBlazorMauiWebView()
+                .UseMauiApp<App>();
+
+            builder.Services.AddBlazorWebView();
+            builder.Services.AddMudServices();
+
+            return builder.Build();
+        }
+    }
+}
