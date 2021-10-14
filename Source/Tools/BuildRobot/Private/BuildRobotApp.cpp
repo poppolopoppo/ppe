ce4abdae5026ca9969ddde441d679cf58bff58db
PopPopOloPpoPo <phamtom.x@gmail.com>
2021-10-14 12:43:53 +0200

    BuildRobot: add a RTTI object to testing remoting

--- Source/Tools/BuildRobot/Private/BuildRobotApp.cpp
+++ Source/Tools/BuildRobot/Private/BuildRobotApp.cpp
@@ -2,13 +2,56 @@
 
 #include "BuildRobotApp.h"
 
-#include "HAL/PlatformNotification.h"
-#include "HAL/PlatformProcess.h"
-#include "HAL/PlatformThread.h"
-
 #include "ApplicationModule.h"
 
+#include "MetaObject.h"
+#include "MetaTransaction.h"
+#include "RTTI/Macros.h"
+#include "RTTI/Macros-impl.h"
+#include "RTTI/Module.h"
+#include "RTTI/Module-impl.h"
+
+#include "HAL/PlatformNotification.h"
+#include "HAL/PlatformProcess.h"
+#include "Time/Timestamp.h"
+
 namespace PPE {
+//----------------------------------------------------------------------------
+//////////////////////////////////////////////////////////////////////////////
+//----------------------------------------------------------------------------
+RTTI_MODULE_DECL(, BuildRobot);
+RTTI_MODULE_DEF(, BuildRobot, MetaObject);
+//----------------------------------------------------------------------------
+using ENotificationIcon = Application::FPlatformNotification::ENotificationIcon;
+namespace Application {
+RTTI_ENUM_HEADER(, ENotificationIcon);
+RTTI_ENUM_BEGIN(BuildRobot, ENotificationIcon)
+RTTI_ENUM_VALUE(None)
+RTTI_ENUM_VALUE(Info)
+RTTI_ENUM_VALUE(Warning)
+RTTI_ENUM_VALUE(Error)
+RTTI_ENUM_END()
+} //!namespace Application
+//----------------------------------------------------------------------------
+class FBuildRobot : public RTTI::FMetaObject {
+    RTTI_CLASS_HEADER(, FBuildRobot, RTTI::FMetaObject);
+
+    FString AppName;
+
+    FTimestamp Now() const {
+        return FTimestamp::Now();
+    }
+
+    void Notify(ENotificationIcon icon, const FWString& title, const FWString& text) const {
+        Application::FPlatformNotification::NotifySystray(icon, title, text);
+    }
+
+};
+RTTI_CLASS_BEGIN(BuildRobot, FBuildRobot, Concrete)
+RTTI_FUNCTION(Notify, (icon, title, text))
+RTTI_FUNCTION(Now, ())
+RTTI_PROPERTY_PUBLIC_FIELD(AppName)
+RTTI_CLASS_END()
 //----------------------------------------------------------------------------
 //////////////////////////////////////////////////////////////////////////////
 //----------------------------------------------------------------------------
@@ -26,7 +69,19 @@ void FBuildRobotApp::Start() {
         L"Worker listening for new tasks",
         L"BuildRobot" );
 
+    RTTI_MODULE(BuildRobot).Start();
+
+    RTTI::FMetaTransaction transaction{ "BuildRobot"_rtti };
+    auto buildRobot = NEW_REF(UserLand, FBuildRobot);
+    buildRobot->RTTI_Export("BuildRobot"_rtti);
+    buildRobot->AppName = Name();
+    transaction.Add(std::move(buildRobot));
+    transaction.LoadAndMount();
+
     ApplicationLoop(); // wait for Remoting events
+
+    transaction.UnmountAndUnload();
+    RTTI_MODULE(BuildRobot).Shutdown();
 }
 //----------------------------------------------------------------------------
 bool FBuildRobotApp::PumpMessages() NOEXCEPT {
