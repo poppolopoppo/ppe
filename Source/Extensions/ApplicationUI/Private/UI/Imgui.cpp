d58039ef3341eba76f22b7efbea330fb8853dd27
PopPopOloPpoPo <phamtom.x@gmail.com>
2022-06-01 00:49:08 +0200

    [ImGui] Ignore warnings in test/shipping builds

--- Source/Extensions/ApplicationUI/Private/UI/Imgui.cpp
+++ Source/Extensions/ApplicationUI/Private/UI/Imgui.cpp
@@ -2,11 +2,25 @@
 
 #include "UI/Imgui.h"
 
+PRAGMA_MSVC_WARNING_PUSH()
+PRAGMA_MSVC_WARNING_DISABLE(4505) // unreferenced function with internal linkage removed
+PRAGMA_MSVC_WARNING_DISABLE(4702) // unreachable code
+
+#ifdef __clang__
+#   pragma clang diagnostic push,
+#endif
+
 #include "External/imgui/imgui.git/imgui.cpp"
 #include "External/imgui/imgui.git/imgui_draw.cpp"
 #include "External/imgui/imgui.git/imgui_tables.cpp"
 #include "External/imgui/imgui.git/imgui_widgets.cpp"
 #include "External/imgui/imgui.git/imgui_demo.cpp"
+
+#ifdef __clang__
+#   pragma clang diagnostic pop
+#endif
+
+PRAGMA_MSVC_WARNING_POP()
 
 namespace PPE {
 namespace Application {
