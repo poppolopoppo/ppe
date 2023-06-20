ee87fad2a6db60bb74c71db3e09f79110cdcc592
PopPopOloPpoPo <phamtom.x@gmail.com>
2023-06-21 00:51:44 +0200

    Build: remove inlined build system before cloning new fork

--- Build/flamegraph.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-go tool pprof -raw -output=cpu.txt cpu.pprof && \
-    ../FlameGraph/stackcollapse-go.pl cpu.txt | ../FlameGraph/flamegraph.pl > cpu.svg
--- Build/hal/windows/bin/UpdateIOWrapper.bat
+++
@@ -1,5 +0,0 @@
-xcopy /y /f /v %~dp0..\..\..\..\Output\Binaries\Tools-IODetouring-Win64-Devel.dll  %~dp0
-xcopy /y /f /v %~dp0..\..\..\..\Output\Binaries\Tools-IODetouring-Win64-Devel.pdb  %~dp0
-
-xcopy /y /f /v %~dp0..\..\..\..\Output\Binaries\Tools-IOWrapper-Win64-Devel.exe  %~dp0
-xcopy /y /f /v %~dp0..\..\..\..\Output\Binaries\Tools-IOWrapper-Win64-Devel.pdb  %~dp0
