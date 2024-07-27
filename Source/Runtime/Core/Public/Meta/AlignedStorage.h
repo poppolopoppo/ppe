380d3e7d53c53f40742a33696ec3e1bc29a46704
poppolopoppo <phamtom.x@gmail.com>
2024-07-28 01:29:08 +0200

    Meta: std::aligned_storage<> was deprecated with C++23
    Use a custom wrapper to restore the previous functionality to avoid deprecation warnings,
    but all clients should be refactored to avoid using the intermediate struct and directly declare them insitu:
    ````
    struct foo {
    	alignas(T) std::byte AlignedStorage[sizeof(T)];
    };

--- Source/Runtime/Core/Public/Meta/AlignedStorage.h
+++ Source/Runtime/Core/Public/Meta/AlignedStorage.h
@@ -7,8 +7,16 @@ namespace Meta {
 //----------------------------------------------------------------------------
 //////////////////////////////////////////////////////////////////////////////
 //----------------------------------------------------------------------------
-template <size_t _SizeInBytes, size_t _Alignement>
-using TAlignedStorage = typename std::aligned_storage<_SizeInBytes, _Alignement>::type;
+#if PPE_HAS_CXX20
+// C++23 has deprecated std::aligned_storage_t... this helper replicates the same functionality
+template <size_t _SizeInBytes, size_t _Alignment = alignof(std::max_align_t)>
+struct TAlignedStorage {
+    alignas(_Alignment) std::byte _Space[_SizeInBytes];
+};
+#else
+template <size_t _SizeInBytes, size_t _Alignment = alignof(std::max_align_t)>
+using TAlignedStorage = typename std::aligned_storage<_SizeInBytes, _Alignment>::type;
+#endif
 //----------------------------------------------------------------------------
 #define ALIGNED_STORAGE(_SIZE_IN_BYTES, _ALIGNMENT) ::PPE::Meta::TAlignedStorage<_SIZE_IN_BYTES, _ALIGNMENT>
 //----------------------------------------------------------------------------
