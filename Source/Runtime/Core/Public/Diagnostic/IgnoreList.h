6a5f0db21d87f6ca62da33dd5a9c23994b37a8d7
poppolopoppo <phamtom.x@gmail.com>
2024-11-27 00:26:18 +0100

    Diagnostic: cleanup exports for FIgnoreList public API

--- Source/Runtime/Core/Public/Diagnostic/IgnoreList.h
+++ Source/Runtime/Core/Public/Diagnostic/IgnoreList.h
@@ -15,10 +15,10 @@ namespace PPE {
 //----------------------------------------------------------------------------
 //////////////////////////////////////////////////////////////////////////////
 //----------------------------------------------------------------------------
-class PPE_CORE_API FIgnoreList : Meta::TSingleton<FIgnoreList> {
+class FIgnoreList : Meta::TSingleton<FIgnoreList> {
     friend class Meta::TSingleton<FIgnoreList>;
     using singleton_type = Meta::TSingleton<FIgnoreList>;
-    static DLL_NOINLINE void* class_singleton_storage() NOEXCEPT; // for shared lib
+    PPE_CORE_API static DLL_NOINLINE void* class_singleton_storage() NOEXCEPT; // for shared lib
 
     FIgnoreList();
 public:
@@ -29,8 +29,8 @@ public:
     struct FIgnoreKey {
         u128 Fingerprint{ PPE_HASH_VALUE_SEED_64, PPE_HASH_VALUE_SEED_64 };
 
-        FIgnoreKey& AppendThread() NOEXCEPT; // seed the key with the thread id => will be specific to this thread
-        FIgnoreKey& Append(FRawMemoryConst key) NOEXCEPT;
+        PPE_CORE_API FIgnoreKey& AppendThread() NOEXCEPT; // seed the key with the thread id => will be specific to this thread
+        PPE_CORE_API FIgnoreKey& Append(FRawMemoryConst key) NOEXCEPT;
 
         template <typename T>
         FIgnoreKey& operator <<(TMemoryView<T> key) NOEXCEPT { return Append(key.template Cast<const u8>()); }
@@ -58,28 +58,31 @@ public:
         }
     };
 
-    void Add(const FIgnoreKey& key);
-    FHitCount Hit(const FIgnoreKey& key); // return > 0 if ignored
-    bool Ignored(const FIgnoreKey& key) const NOEXCEPT;
-    void Clear();
+    struct FIgnoreScope {
+        PPE_CORE_API FIgnoreScope() NOEXCEPT;
+        PPE_CORE_API ~FIgnoreScope();
+    };
+
+    PPE_CORE_API void Add(const FIgnoreKey& key);
+    NODISCARD PPE_CORE_API FHitCount Hit(const FIgnoreKey& key); // return > 0 if ignored
+    NODISCARD PPE_CORE_API bool Ignored(const FIgnoreKey& key) const NOEXCEPT;
+    PPE_CORE_API void Clear();
 
     // the static variants won't fail if the singleton is not available
-    static bool Available();
-    static void AddIFP(const FIgnoreKey& key);
-    static FHitCount HitIFP(const FIgnoreKey& key); // return > 0 if ignored
-    static bool IgnoredIFP(const FIgnoreKey& key) NOEXCEPT;
-    static void ClearIFP();
+    NODISCARD PPE_CORE_API static bool Available();
+    PPE_CORE_API static void AddIFP(const FIgnoreKey& key);
+    NODISCARD PPE_CORE_API static FHitCount HitIFP(const FIgnoreKey& key); // return > 0 if ignored
+    NODISCARD PPE_CORE_API static bool IgnoredIFP(const FIgnoreKey& key) NOEXCEPT;
+    PPE_CORE_API static void ClearIFP();
 
 public: // singleton API
     using singleton_type::Get;
 #if USE_PPE_ASSERT
     using singleton_type::HasInstance;
 #endif
-    using singleton_type::Destroy;
 
-    static void Create() {
-        singleton_type::Create();
-    }
+    PPE_CORE_API static void Create();
+    PPE_CORE_API static void Destroy();
 
 private:
     using FHitMap = MAP(Diagnostic, FIgnoreKey, FHitCount);
